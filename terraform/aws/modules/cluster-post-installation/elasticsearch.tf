638af8f6f234488694b75e3630345f0cf1d88f3d
Stylianos Rigas <stylianosrigas@gmail.com>
2019-06-24 11:20:50 +0100

    MM-16306 Adding fluentd deployment in central monitoring cluster. Fixing namespaces for kibana and elasticsearch.

--- terraform/aws/modules/cluster-post-installation/elasticsearch.tf
+++ terraform/aws/modules/cluster-post-installation/elasticsearch.tf
@@ -1,12 +1,9 @@
 resource "helm_release" "elasticsearch" {
   name       = "mattermost-cm-elasticsearch"
-  namespace  = "monitoring"
+  namespace  = "logging"
   repository = "${data.helm_repository.stable.metadata.0.name}"
   chart      = "stable/elasticsearch"
   values = [
     "${file("../../../../../chart-values/elasticsearch_values.yaml")}"
-  ]
-  depends_on = [
-    "kubernetes_namespace.monitoring"
   ]
 }
--- terraform/aws/modules/cluster-post-installation/fluentd.tf
+++ terraform/aws/modules/cluster-post-installation/fluentd.tf
@@ -2,8 +2,8 @@ resource "helm_release" "kibana" {
   name       = "mattermost-cm-kibana"
   namespace  = "logging"
   repository = "${data.helm_repository.stable.metadata.0.name}"
-  chart      = "stable/kibana"
+  chart      = "stable/fluentd-elasticsearch"
   values = [
-    "${file("../../../../../chart-values/kibana_values.yaml")}"
+    "${file("../../../../../chart-values/fluentd-elasticsearch_values.yaml")}"
   ]
 }
