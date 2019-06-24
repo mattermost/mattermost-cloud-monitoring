4919f4411386c8476dd3d9242059b4f4de858a78
Stylianos Rigas <stylianosrigas@gmail.com>
2019-06-24 09:56:56 +0100

    MM-16241 Code improvements and adding some tests.

--- prometheus-dns-registration-service/main.go
+++ prometheus-dns-registration-service/main.go
@@ -14,8 +14,8 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/eks"
@@ -29,11 +29,12 @@ import (
 	clientset "k8s.io/client-go/kubernetes"
 )
 
+// This structure is used to decode the configmap
 type config struct {
 	Global struct {
 		EvaluationInterval string `yaml:"evaluation_interval"`
-		ScrapeInterval string `yaml:"scrape_interval"`
-		ScrapeTimeOut string `yaml:"scrape_timeout"`
+		ScrapeInterval     string `yaml:"scrape_interval"`
+		ScrapeTimeOut      string `yaml:"scrape_timeout"`
 	} `yaml:"global"`
 	RuleFiles     []string `yaml:"rule_files"`
 	ScrapeConfigs []struct {
@@ -50,14 +51,14 @@ type config struct {
 	} `yaml:"scrape_configs"`
 }
 
-type ClusterConfig struct {
+type clusterConfig struct {
 	ClusterName              string
 	MasterEndpoint           string
 	CertificateAuthorityData string
 	Session                  *session.Session
 }
 
-type ClientConfig struct {
+type clientConfig struct {
 	Client      *clientcmdapi.Config
 	ClusterName string
 	ContextName string
@@ -71,11 +72,11 @@ func main() {
 
 func handler() {
 
-	hostedZoneID := "ZWY9TDHREDEV4"
+	hostedZoneID := os.Getenv("HOSTED_ZONE_ID")
 	clusterName := os.Getenv("CLUSTER_NAME")
-	configMapName := "mattermost-cm-prometheus-server"
+	configMapName := os.Getenv("CONFIG_MAP_NAME")
 
-	log.Info("Getting k8s client...")
+	log.Info("Getting the k8s client")
 	clientSet, err := k8sClient(clusterName)
 	if err != nil {
 		log.Fatal(err)
@@ -89,34 +90,27 @@ func handler() {
 	}
 	log.Info("Successfully got the Prometheus server configmap")
 
-	data := []byte(configMap.Data["prometheus.yml"])
-	
+	data := (configMap.Data["prometheus.yml"])
 	C := config{}
-
 	log.Info("Decoding configmap data into structure")
-	err = yaml.Unmarshal(data, &C)
+	C, err = decodeConfig(data, C)
 	if err != nil {
 		log.Fatalf("Unable to decode into struct, %v", err)
 	}
 	log.Info("Successfully decoded configmap data into structure")
 
-	
 	log.Info("Getting existing Route53 records")
 	targets, err := getRoute53Records(hostedZoneID)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Info("Replacing existing targets with updated values")
-	C.ScrapeConfigs[0].StaticConfigs[0].Targets = targets
-	
-	newData, err := yaml.Marshal(&C)
+	dataNew, err := updateTargets(C, targets)
 	if err != nil {
-		log.Fatalf("error: %v", err)
+		log.Fatal(err)
 	}
+	configMap.Data["prometheus.yml"] = string([]byte(dataNew))
 
-	log.Info("Updating Prometheus server configmap with new values")
-	configMap.Data["prometheus.yml"] = string([]byte(newData))
 	_, err = clientSet.CoreV1().ConfigMaps("monitoring").Update(configMap)
 	if err != nil {
 		log.Fatal(err)
@@ -124,11 +118,34 @@ func handler() {
 	log.Info("Successfully updated Prometheus server configmap")
 }
 
+// updateTargets is used to replace existing configmap Prometheus targets with new ones
+func updateTargets(C config, targets []string) ([]byte, error) {
+	log.Info("Replacing existing targets with updated values")
+	C.ScrapeConfigs[0].StaticConfigs[0].Targets = targets
+	data, err := yaml.Marshal(&C)
+	if err != nil {
+		return nil, err
+	}
+	log.Info("Updating Prometheus server configmap with new values")
+	return data, nil
+}
+
+// decodeConfig is used to decode the configmap data into a usable structure
+func decodeConfig(data string, C config) (config, error) {
+	dataByte := []byte(data)
+	err := yaml.Unmarshal(dataByte, &C)
+	if err != nil {
+		return C, err
+	}
+	return C, nil
+}
+
+// k8sClient is used to get the k8s client 
 func k8sClient(clusterName string) (*clientset.Clientset, error) {
 	session := newSession()
 
 	// Setup the basic EKS cluster info
-	cfg := &ClusterConfig{
+	cfg := &clusterConfig{
 		ClusterName: clusterName,
 		Session:     session,
 	}
@@ -152,7 +169,8 @@ func k8sClient(clusterName string) (*clientset.Clientset, error) {
 	return clientSet, nil
 }
 
-func (c *ClusterConfig) loadConfig() error {
+// loadConfig is used to load the EKS configuration
+func (c *clusterConfig) loadConfig() error {
 	svc := eks.New(c.Session)
 	input := &eks.DescribeClusterInput{
 		Name: aws.String(c.ClusterName),
@@ -171,7 +189,8 @@ func (c *ClusterConfig) loadConfig() error {
 	return nil
 }
 
-func (c *ClusterConfig) newClientConfig() (*ClientConfig, error) {
+// newClientConfig is used to create the k8s client configuration that will be used for the authenication
+func (c *clusterConfig) newClientConfig() (*clientConfig, error) {
 
 	stsAPI := sts.New(c.Session)
 
@@ -187,7 +206,7 @@ func (c *ClusterConfig) newClientConfig() (*ClientConfig, error) {
 	}
 
 	log.Info("Creating Kubernetes client config")
-	clientConfig := &ClientConfig{
+	clientConfig := &clientConfig{
 		Client: &clientcmdapi.Config{
 			Clusters: map[string]*clientcmdapi.Cluster{
 				c.ClusterName: {
@@ -214,6 +233,7 @@ func (c *ClusterConfig) newClientConfig() (*ClientConfig, error) {
 	return clientConfig, nil
 }
 
+// newSession creates a new STS session
 func newSession() *session.Session {
 	config := aws.NewConfig()
 	config = config.WithCredentialsChainVerboseErrors(true)
@@ -229,6 +249,7 @@ func newSession() *session.Session {
 	return session.Must(session.NewSessionWithOptions(opts))
 }
 
+// checkAuth checks the AWS access
 func checkAuth(stsAPI stsiface.STSAPI) (string, error) {
 	input := &sts.GetCallerIdentityInput{}
 	output, err := stsAPI.GetCallerIdentity(input)
@@ -240,6 +261,7 @@ func checkAuth(stsAPI stsiface.STSAPI) (string, error) {
 	return iamRoleARN, nil
 }
 
+// getUsername get the username out of the AWS Lambda role arn
 func getUsername(iamRoleARN string) string {
 	usernameParts := strings.Split(iamRoleARN, "/")
 	if len(usernameParts) > 1 {
@@ -248,7 +270,8 @@ func getUsername(iamRoleARN string) string {
 	return "iam-root-account"
 }
 
-func (c *ClientConfig) withEmbeddedToken() (*ClientConfig, error) {
+// withEmbeddedToken create a new AWS token 
+func (c *clientConfig) withEmbeddedToken() (*clientConfig, error) {
 	clientConfigCopy := *c
 
 	log.Info("Generating token")
@@ -270,7 +293,8 @@ func (c *ClientConfig) withEmbeddedToken() (*ClientConfig, error) {
 	return &clientConfigCopy, nil
 }
 
-func (c *ClientConfig) newClientSetWithEmbeddedToken() (*clientset.Clientset, *rest.Config, error) {
+// newClientSetWithEmbeddedToken creates the new k8s client
+func (c *clientConfig) newClientSetWithEmbeddedToken() (*clientset.Clientset, *rest.Config, error) {
 	clientConfig, err := c.withEmbeddedToken()
 	if err != nil {
 		return nil, nil, err
@@ -284,7 +308,8 @@ func (c *ClientConfig) newClientSetWithEmbeddedToken() (*clientset.Clientset, *r
 	return clientSet, config, err
 }
 
-func (c *ClientConfig) newClientSet() (*clientset.Clientset, *rest.Config, error) {
+// newClientSet is used by newClientSetWithEmbeddedToken to create the k8s client
+func (c *clientConfig) newClientSet() (*clientset.Clientset, *rest.Config, error) {
 	clientConfig, err := clientcmd.NewDefaultClientConfig(*c.Client, &clientcmd.ConfigOverrides{}).ClientConfig()
 	if err != nil {
 		return nil, clientConfig, err
@@ -297,6 +322,7 @@ func (c *ClientConfig) newClientSet() (*clientset.Clientset, *rest.Config, error
 	return client, clientConfig, err
 }
 
+// getRoute53Records is used to get the existing Route53 Records
 func getRoute53Records(hostedZoneID string) ([]string, error) {
 	sess, err := session.NewSession(&aws.Config{})
 	if err != nil {
@@ -313,13 +339,20 @@ func getRoute53Records(hostedZoneID string) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
-	matchingRecords := []string{}
+	matchingRecords := getMatchingRecords(recordSets.ResourceRecordSets)
 
-	for _, record := range recordSets.ResourceRecordSets {
+	return matchingRecords, nil
+}
+
+// getMatchingRecords is used to get only Prometheus related Route53 records.
+func getMatchingRecords(recordSets []*route53.ResourceRecordSet) []string {
+	matchingRecords := []string{}
+	fmt.Println(recordSets)
+	for _, record := range recordSets {
 		if strings.Contains(*record.Name, ".prometheus.internal") {
 			matchingRecords = append(matchingRecords, *record.Name)
 		}
 	}
 	log.Info("Returning matching Route53 records")
-	return matchingRecords, nil
+	return matchingRecords
 }
