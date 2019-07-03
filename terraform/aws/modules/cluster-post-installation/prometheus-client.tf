46dfa92c2001ccfc1d3b7f2161b60faede8e244c
Stylianos Rigas <stylianosrigas@gmail.com>
2019-07-03 17:30:51 +0100

    MM-16549 Adding prometheus client configuration for central command and control cluster

---
+++ terraform/aws/modules/cluster-post-installation/prometheus-client.tf
@@ -0,0 +1,13 @@
+resource "helm_release" "prometheus-client" {
+  name       = "mattermost-cm-prometheus-client"
+  namespace  = "monitoring"
+  repository = "${data.helm_repository.stable.metadata.0.name}"
+  chart      = "stable/prometheus"
+  wait       = false
+  values = [
+    "${file("../../../../../chart-values/prometheus-client_values.yaml")}"
+  ]
+  depends_on = [
+    "kubernetes_namespace.monitoring"
+  ]
+}
