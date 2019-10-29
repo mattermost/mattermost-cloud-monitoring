aa946165b8f6add6389dd29993a406f7a912da4d
Stylianos Rigas <stylianosrigas@gmail.com>
2019-10-29 10:54:08 +0000

    MM-18123 Moving monitoring tooling endpoints to use internal ELBs (#25)

--- terraform/aws/environments/test/cluster-post-installation/main.tf
+++ terraform/aws/environments/test/cluster-post-installation/main.tf
@@ -20,6 +20,12 @@ module "cluster-post-installation" {
   region = "${var.region}"
   tiller_version = "${var.tiller_version}"
   kubeconfig_dir = "${var.kubeconfig_dir}"
+  grafana_tls_crt = "${var.grafana_tls_crt}"
+  grafana_tls_key = "${var.grafana_tls_key}"
+  prometheus_tls_crt = "${var.prometheus_tls_crt}"
+  prometheus_tls_key = "${var.prometheus_tls_key}"
+  kibana_tls_crt = "${var.kibana_tls_crt}"
+  kibana_tls_key = "${var.kibana_tls_key}"
   providers = {
     aws = "aws.post-deployment"
   }
--- terraform/aws/modules/route53-registration/route53.tf
+++ terraform/aws/modules/route53-registration/route53.tf
@@ -14,35 +14,35 @@ data "kubernetes_service" "nginx" {
 
 #Public records
 resource "aws_route53_record" "prometheus" {
-  zone_id = "${var.public_hosted_zoneid}"
+  zone_id = "${var.private_hosted_zoneid}"
   name    = "prometheus"
   type    = "CNAME"
   ttl     = "60"
-  records = ["${data.kubernetes_service.nginx.load_balancer_ingress.0.hostname}"]
+  records = ["${data.kubernetes_service.nginx-internal.load_balancer_ingress.0.hostname}"]
 }
 
 resource "aws_route53_record" "grafana" {
-  zone_id = "${var.public_hosted_zoneid}"
+  zone_id = "${var.private_hosted_zoneid}"
   name    = "grafana"
   type    = "CNAME"
   ttl     = "60"
-  records = ["${data.kubernetes_service.nginx.load_balancer_ingress.0.hostname}"]
+  records = ["${data.kubernetes_service.nginx-internal.load_balancer_ingress.0.hostname}"]
 }
 
 resource "aws_route53_record" "kibana" {
-  zone_id = "${var.public_hosted_zoneid}"
+  zone_id = "${var.private_hosted_zoneid}"
   name    = "kibana"
   type    = "CNAME"
   ttl     = "60"
-  records = ["${data.kubernetes_service.nginx.load_balancer_ingress.0.hostname}"]
+  records = ["${data.kubernetes_service.nginx-internal.load_balancer_ingress.0.hostname}"]
 }
 
 resource "aws_route53_record" "auth" {
-  zone_id = "${var.public_hosted_zoneid}"
+  zone_id = "${var.private_hosted_zoneid}"
   name    = "auth"
   type    = "CNAME"
   ttl     = "60"
-  records = ["${data.kubernetes_service.nginx.load_balancer_ingress.0.hostname}"]
+  records = ["${data.kubernetes_service.nginx-internal.load_balancer_ingress.0.hostname}"]
 }
 
 resource "aws_route53_record" "elasticsearch" {
