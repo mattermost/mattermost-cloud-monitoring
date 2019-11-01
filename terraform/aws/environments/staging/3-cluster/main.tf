38db2b77fa0bfba0bc47ff322e45ba08e5fe11fb
Stylianos Rigas <stylianosrigas@gmail.com>
2019-11-01 15:09:52 +0000

    MM-18938 Enforcing use of 0.12 Terraform version

--- terraform/aws/environments/staging/3-cluster/main.tf
+++ terraform/aws/environments/staging/3-cluster/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.11"
+  required_version = ">= 0.12"
   backend "s3" {
     bucket = "terraform-cloud-monitoring-state-bucket-staging"
     key    = "mattermost-central-command-control"
