196451dbb541bc94af2169c8b3f06bc5bc69e6e6
JoramWilander <jwawilander@gmail.com>
2019-11-04 15:52:49 -0500

    Start implementing lambda to setup transit gateway attachment for newly created VPCs

---
+++ post-vpc-setup/go.mod
@@ -0,0 +1,11 @@
+module github.com/mattermost/mattermost-cloud-monitoring/post-vpc-setup
+
+go 1.12
+
+require (
+	github.com/aws/aws-lambda-go v1.13.2
+	github.com/aws/aws-sdk-go v1.25.26
+	github.com/pkg/errors v0.8.1
+	github.com/sirupsen/logrus v1.4.2
+	golang.org/x/net v0.0.0-20191101175033-0deb6923b6d9 // indirect
+)
---
+++ post-vpc-setup/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudtrail"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/pkg/errors"
+
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	transitGatewayEnv = "TRANSIT_GATEWAY_ID"
+)
+
+// CreateVPCEvent extends the CloudTrail event to be specific to the CreateVPC event
+type CreateVPCEvent struct {
+	cloudtrail.Event
+
+	ResponseElements struct {
+		RequestID string `json:"requestId"`
+		VPC       struct {
+			ID                      string `json:"vpcId"`
+			State                   string `json:"state"`
+			OwnerID                 string `json:"ownerId"`
+			CidrBlock               string `json:"cidrBlock"`
+			CidrBlockAssociationSet struct {
+				Items []struct {
+					CidrBlock      string `json:"cidrBlock"`
+					AssociationID  string `json:"associationId"`
+					CidrBlockState struct {
+						State string `json:"state"`
+					} `json:"cidrBlockState"`
+				} `json:"items"`
+			} `json:"cidrBlockAssociationSet"`
+			Ipv6CidrBlockAssociationSet struct {
+			} `json:"ipv6CidrBlockAssociationSet"`
+			DhcpOptionsID   string `json:"dhcpOptionsId"`
+			InstanceTenancy string `json:"instanceTenancy"`
+			TagSet          struct {
+			} `json:"tagSet"`
+			IsDefault bool `json:"isDefault"`
+		} `json:"vpc"`
+	} `json:"responseElements"`
+}
+
+func postVPCCreation(ctx context.Context, cloudWatchEvent events.CloudWatchEvent) {
+	transitGatewayID := os.Getenv(transitGatewayEnv)
+	if len(transitGatewayID) == 0 {
+		log.Fatal(transitGatewayEnv + " environment variable is not set.")
+		return
+	}
+
+	event := &CreateVPCEvent{}
+	err := json.Unmarshal(cloudWatchEvent.Detail, event)
+	if err != nil {
+		log.WithError(err).Fatal("Unable to unmarshal event details")
+	}
+
+	log.Infof("New VPC created with id: %s", event.ResponseElements.VPC.ID)
+
+	err = attachToTransitGateway(event.ResponseElements.VPC.ID, transitGatewayID)
+	if err != nil {
+		log.WithError(err).Fatal("Unable to create transit gateway attachment")
+	}
+
+}
+
+func attachToTransitGateway(vpcID, transitGatewayID string) error {
+	sess, err := session.NewSession(&aws.Config{})
+	if err != nil {
+		return err
+	}
+
+	svc := ec2.New(sess)
+
+	subnetInput := &ec2.DescribeSubnetsInput{
+		Filters: []*ec2.Filter{
+			{
+				Name: aws.String("vpc-id"),
+				Values: []*string{
+					aws.String(vpcID),
+				},
+			},
+		},
+	}
+
+	subnetResults, err := svc.DescribeSubnets(subnetInput)
+	if err != nil {
+		return err
+	}
+
+	if len(subnetResults.Subnets) == 0 {
+		return errors.New("VPC has no subnets")
+	}
+
+	subnetIDs := make([]*string, len(subnetResults.Subnets))
+	for i, subnet := range subnetResults.Subnets {
+		subnetIDs[i] = subnet.SubnetId
+	}
+
+	log.Infof("Subnets to attach: %v", subnetIDs)
+
+	attachmentInput := &ec2.CreateTransitGatewayVpcAttachmentInput{
+		VpcId:            aws.String(vpcID),
+		SubnetIds:        subnetIDs,
+		TransitGatewayId: aws.String(transitGatewayID),
+	}
+
+	_, err = svc.CreateTransitGatewayVpcAttachment(attachmentInput)
+	if err != nil {
+		return errors.Wrap(err, "error attaching VPC to transit gateway")
+	}
+
+	return nil
+}
+
+func main() {
+	lambda.Start(postVPCCreation)
+}
