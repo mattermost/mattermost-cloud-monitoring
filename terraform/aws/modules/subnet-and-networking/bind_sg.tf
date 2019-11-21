cb19657da5ba51e476638228effc20d6e7c5c567
Stylianos Rigas <stylianosrigas@gmail.com>
2019-11-21 11:19:33 +0000

    MM-18124 Adding support for automatic updates of the Bind SG with new VPCs

---
+++ terraform/aws/modules/subnet-and-networking/bind_sg.tf
@@ -0,0 +1,28 @@
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket   = "terraform-cloud-monitoring-state-bucket-test"
+    key      = "mattermost-network"
+    region   = "us-east-1"
+  }
+}
+
+resource "aws_security_group_rule" "master_egress_udp" {
+  cidr_blocks       = var.vpc_cidrs
+  description       = "Inbound Traffic from Provisioning VPC"
+  from_port         = 53
+  protocol          = "udp"
+  security_group_id = data.terraform_remote_state.network.outputs.bind_sg
+  to_port           = 53
+  type              = "ingress"
+}
+
+resource "aws_security_group_rule" "master_egress_tcp" {
+  cidr_blocks       = var.vpc_cidrs
+  description       = "Inbound Traffic from Provisioning VPC"
+  from_port         = 53
+  protocol          = "tcp"
+  security_group_id = data.terraform_remote_state.network.outputs.bind_sg
+  to_port           = 53
+  type              = "ingress"
+}
