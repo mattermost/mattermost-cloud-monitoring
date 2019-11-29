c9985ab35ec8dd9d6b154cf6dcef072dc47bf035
Carlos Panato <ctadeu@gmail.com>
2019-11-29 10:59:30 +0100

    add main.tf for stg

--- terraform/aws/environments/staging/4-command-and-control/3-cluster-post-installation/main.tf
+++ terraform/aws/environments/staging/4-command-and-control/3-cluster-post-installation/main.tf
@@ -7,7 +7,6 @@ terraform {
   }
 }
 
-
 provider "aws" {
   region = var.region
   alias  = "post-deployment"
@@ -27,7 +26,33 @@ module "cluster-post-installation" {
   prometheus_tls_key = var.prometheus_tls_key
   kibana_tls_crt = var.kibana_tls_crt
   kibana_tls_key = var.kibana_tls_key
+  db_identifier = var.db_identifier
+  allocated_db_storage = var.allocated_db_storage
+  db_engine_version = var.db_engine_version
+  db_instance_class = var.db_instance_class
+  db_name = var.db_name
+  db_username = var.db_username
+  db_password = var.db_password
+  db_backup_retention_period = var.db_backup_retention_period
+  db_backup_window = var.db_backup_window
+  db_maintenance_window = var.db_maintenance_window
+  vpc_id = var.vpc_id
+  private_subnets = var.private_subnets
+  mattermost_cloud_image = var.mattermost_cloud_image
+  mattermost_cloud_ingress = var.mattermost_cloud_ingress
+  snapshot_identifier = var.snapshot_identifier
+  mattermost-cloud-namespace = var.mattermost-cloud-namespace
+  mattermost_cloud_secret_ssh_private = var.mattermost_cloud_secret_ssh_private
+  mattermost_cloud_secret_ssh_public = var.mattermost_cloud_secret_ssh_public
+  mattermost_cloud_secrets_aws_access_key = var.mattermost_cloud_secrets_aws_access_key
+  mattermost_cloud_secrets_aws_secret_key = var.mattermost_cloud_secrets_aws_secret_key
+  mattermost_cloud_secrets_aws_region = var.mattermost_cloud_secrets_aws_region
+  mattermost_cloud_secrets_certificate_aws_arn = var.mattermost_cloud_secrets_certificate_aws_arn
+  mattermost_cloud_secrets_private_dns = var.mattermost_cloud_secrets_private_dns
+  mattermost_cloud_secrets_private_route53_id = var.mattermost_cloud_secrets_private_route53_id
+  mattermost_cloud_secrets_route53_id = var.mattermost_cloud_secrets_route53_id
+
   providers = {
-    aws = "aws.post-deployment"
+    aws = aws.post-deployment
   }
 }
