8eff573faa45de884a2d05beefabeb7eb390d83d
Stylianos Rigas <stylianosrigas@gmail.com>
2020-01-07 10:08:29 +0000

    MM-21522 Route table improvements and ignore tag configuration for private provisioning subnets

--- terraform/aws/environments/prod/1-network/main.tf
+++ terraform/aws/environments/prod/1-network/main.tf
@@ -43,7 +43,8 @@ module "tgw_attachment" {
   subnet_ids = module.shared_services_vpc.private_subnets
   transit_gateway_id = var.transit_gateway_id
   vpc_id = module.shared_services_vpc.vpc_id
-  route_table_id = module.shared_services_vpc.private_route_table_ids[0]
+  private_route_table_id = module.shared_services_vpc.private_route_table_ids[0]
+  public_route_table_id = module.shared_services_vpc.public_route_table_ids[0]
   transit_gtw_route_destination = var.transit_gtw_route_destination
 }
 
@@ -100,7 +101,8 @@ module "tgw_attachment_command_control" {
   subnet_ids = module.command_control_vpc.private_subnets
   transit_gateway_id = var.transit_gateway_id
   vpc_id = module.command_control_vpc.vpc_id
-  route_table_id = module.command_control_vpc.private_route_table_ids[0]
+  private_route_table_id = module.command_control_vpc.private_route_table_ids[0]
+  public_route_table_id = module.command_control_vpc.public_route_table_ids[0]
   transit_gtw_route_destination = var.transit_gtw_route_destination
 }
 
@@ -133,7 +135,8 @@ module "tgw_attachment_auth" {
   subnet_ids = module.auth_vpc.private_subnets
   transit_gateway_id = var.transit_gateway_id
   vpc_id = module.auth_vpc.vpc_id
-  route_table_id = module.auth_vpc.private_route_table_ids[0]
+  private_route_table_id = module.auth_vpc.private_route_table_ids[0]
+  public_route_table_id = module.auth_vpc.public_route_table_ids[0]
   transit_gtw_route_destination = var.transit_gtw_route_destination
 }
 
