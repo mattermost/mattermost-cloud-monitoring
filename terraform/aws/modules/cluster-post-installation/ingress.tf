5895b1928d68501cfeaed226cef760a771770651
Stylianos Rigas <stylianosrigas@gmail.com>
2020-01-13 16:23:07 +0000

    MM-19103 Turning C&C API private and removing non used public NGINX

--- terraform/aws/modules/cluster-post-installation/ingress.tf
+++
@@ -1,9 +0,0 @@
-resource "helm_release" "nginx" {
-  name       = "mattermost-cm-nginx"
-  namespace  = "network"
-  repository = data.helm_repository.stable.metadata.0.name
-  chart      = "stable/nginx-ingress"
-  depends_on = [
-    kubernetes_namespace.network
-  ]
-}
