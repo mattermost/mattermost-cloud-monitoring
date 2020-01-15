841f8f782c6b95803ad7aa32164707e246a685a4
Stylianos Rigas <stylianosrigas@gmail.com>
2020-01-15 09:39:27 +0000

    MM-19103 Turning C&C API private and removing non used public NGINX (#49)

--- terraform/aws/modules/cluster/master.tf
+++ terraform/aws/modules/cluster/master.tf
@@ -12,7 +12,7 @@ resource "aws_eks_cluster" "cluster" {
     security_group_ids = [aws_security_group.cluster-sg.id]
     subnet_ids         = flatten([var.public_subnet_ids, var.private_subnet_ids])
     endpoint_private_access = true
-    endpoint_public_access = true
+    endpoint_public_access = false
   }
 
   depends_on = [
