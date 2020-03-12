1677a5df0bef962b7415877b7322f030e61cbfc9
Stylianos Rigas <stylianosrigas@gmail.com>
2020-03-12 16:43:28 +0000

    MM-22281 Adding gitlab and eks modules, gitlab deployment configuration (#70)
    * MM-22281 Adding gitlab and eks modules, gitlab deployment configuration
    
    * MM-22281 Removing hardcoded parameters for s3 bucket names
    
    * MM-22281 Adding multiaz option for database

---
+++ terraform/aws/modules/eks-cluster/worker_ingress.tf
@@ -0,0 +1,11 @@
+############Worker Node Access to EKS Master Cluste#############
+
+resource "aws_security_group_rule" "worker-ingress" {
+  description              = "Allow pods to communicate with the cluster API Server"
+  from_port                = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.cluster-sg.id
+  source_security_group_id = aws_security_group.worker-sg.id
+  to_port                  = 443
+  type                     = "ingress"
+}
