3688975a81295f137cdfe1dfc653b37a7e145eee
Stylianos Rigas <stylianosrigas@gmail.com>
2020-04-21 14:57:03 +0300

    Adding variable for nginx chart values directory

--- terraform/aws/modules/public-ingress/nginx-public.tf
+++ terraform/aws/modules/public-ingress/nginx-public.tf
@@ -15,7 +15,7 @@ resource "helm_release" "public_nginx" {
   chart     = "stable/nginx-ingress"
   namespace = "nginx-public"
   values = [
-    "${file("../../../modules/public-ingress/nginx_values.yaml")}"
+    "${file(var.nginx_chart_values_directory)}"
   ]
 
   set {
--- terraform/aws/modules/public-ingress/variables.tf
+++ terraform/aws/modules/public-ingress/variables.tf
@@ -1,3 +1,5 @@
 variable "deployment_name" {}
 
 variable "kubeconfig_dir" {}
+
+variable "nginx_chart_values_directory" {}
