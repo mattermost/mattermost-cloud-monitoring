5496d9957ed93f9a4dc678fd44356fbb56d5c8dc
Angelos Kyratzakos <angelos.kyratzakos@mattermost.com>
2020-04-21 15:28:36 +0300

    add dev env configuration - comamnd and control (#92)

---
+++ terraform/aws/environments/dev/4-command-and-control/4-route53-registration/variables.tf
@@ -0,0 +1,18 @@
+variable "deployment_name" {
+  default = "mattermost-central-command-control"
+  type    = string
+}
+variable "region" {
+  default = "us-east-1"
+  type    = string
+}
+
+variable "kubeconfig_dir" {
+  default = "$HOME/generated"
+  type    = string
+}
+
+variable "private_hosted_zoneid" {
+  default = ""
+  type    = string
+}
