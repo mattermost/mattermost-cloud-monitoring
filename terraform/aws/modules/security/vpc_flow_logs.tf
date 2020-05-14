d6791b387ec93292cedd9fed1b4cb9fba3a22c2c
Angelos Kyratzakos <angelos.kyratzakos@mattermost.com>
2020-05-14 10:48:44 +0300

    enabling S3 default encryption on vpc flow logs (#114)

--- terraform/aws/modules/security/vpc_flow_logs.tf
+++ terraform/aws/modules/security/vpc_flow_logs.tf
@@ -1,5 +1,9 @@
 data "aws_vpcs" "vpcs" {}
 
+data "aws_kms_alias" "s3" {
+  name = "alias/aws/s3"
+}
+
 resource "aws_flow_log" "cloud_vpc" {
   count                = length(data.aws_vpcs.vpcs.ids)
   log_destination      = aws_s3_bucket.vpc_flow_logs.arn
@@ -12,4 +16,12 @@ resource "aws_s3_bucket" "vpc_flow_logs" {
   bucket = "mattermost-vpc-flow-logs-${var.environment}"
   region = "us-east-1"
   acl    = "private"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = data.aws_kms_alias.s3.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 }
