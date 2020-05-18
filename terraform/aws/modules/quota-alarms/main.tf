14063f4bf421f04cbf27b42dbfb904656fd90bbd
Stylianos Rigas <stylianosrigas@gmail.com>
2020-05-18 10:59:57 +0300

    MM-25291 Running TF FMT

--- terraform/aws/modules/quota-alarms/main.tf
+++ terraform/aws/modules/quota-alarms/main.tf
@@ -4,16 +4,16 @@ data "aws_caller_identity" "current" {}
 resource "aws_cloudwatch_metric_alarm" "grafana_metrics" {
   for_each = toset(var.metrics)
 
-  alarm_name                = "${each.value}-quota-alarm"
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = "1"
-  metric_name               = each.value
-  namespace                 = "AWS/Grafana"
-  period                    = "3600"
-  statistic                 = "Average"
-  threshold                 = var.alarm_threshold
-  alarm_description         = "This metric monitors ${each.value} utilization"
-  actions_enabled           = "true"
-  alarm_actions             = ["arn:aws:sns:us-east-1:${data.aws_caller_identity.current.account_id}:elb-alarm-topic"]
-  ok_actions                = ["arn:aws:sns:us-east-1:${data.aws_caller_identity.current.account_id}:elb-alarm-topic"]
+  alarm_name          = "${each.value}-quota-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = each.value
+  namespace           = "AWS/Grafana"
+  period              = "3600"
+  statistic           = "Average"
+  threshold           = var.alarm_threshold
+  alarm_description   = "This metric monitors ${each.value} utilization"
+  actions_enabled     = "true"
+  alarm_actions       = ["arn:aws:sns:us-east-1:${data.aws_caller_identity.current.account_id}:elb-alarm-topic"]
+  ok_actions          = ["arn:aws:sns:us-east-1:${data.aws_caller_identity.current.account_id}:elb-alarm-topic"]
 }
