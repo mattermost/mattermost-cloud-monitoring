a6b656d72689ca16c3275e89a5520b512c0747be
Stylianos Rigas <stylianosrigas@gmail.com>
2020-05-26 18:28:31 +0300

    Running Terraform FMT

--- terraform/aws/modules/subnet-and-networking/security_groups.tf
+++ terraform/aws/modules/subnet-and-networking/security_groups.tf
@@ -145,11 +145,11 @@ resource "aws_security_group_rule" "db_ingress_worker" {
 resource "aws_security_group_rule" "db_ingress_worker_command_control" {
   for_each = toset(var.vpc_cidrs)
 
-  cidr_blocks              = var.command_and_control_private_subnet_cidrs
-  description              = "Ingress Traffic from Command and Control Private Subnets"
-  from_port                = 3306
-  protocol                 = "TCP"
-  security_group_id        = aws_security_group.db_sg[each.value]["id"]
-  to_port                  = 3306
-  type                     = "ingress"
+  cidr_blocks       = var.command_and_control_private_subnet_cidrs
+  description       = "Ingress Traffic from Command and Control Private Subnets"
+  from_port         = 3306
+  protocol          = "TCP"
+  security_group_id = aws_security_group.db_sg[each.value]["id"]
+  to_port           = 3306
+  type              = "ingress"
 }
