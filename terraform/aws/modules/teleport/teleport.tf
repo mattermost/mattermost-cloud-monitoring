9af82489956b1e7b9cf0d4018905cf31b38c327e
Stylianos Rigas <stylianosrigas@gmail.com>
2020-06-05 16:42:41 +0300

    MM-24189 Adding Terraform module to deploy Teleport application in k8s clusters

---
+++ terraform/aws/modules/teleport/teleport.tf
@@ -0,0 +1,21 @@
+resource "helm_release" "teleport" {
+  name       = "teleport"
+  namespace  = "teleport"
+  repository = data.helm_repository.chartmuseum.metadata.0.name
+  chart      = "chartmuseum/teleport"
+
+  set_string {
+    name  = "config.auth_service.cluster_name"
+    value = "cloud-${var.environment}-${var.cluster_name}"
+  }
+  depends_on = [
+    kubernetes_namespace.teleport,
+  ]
+}
+
+
+resource "kubernetes_namespace" "teleport" {
+  metadata {
+    name = "teleport"
+  }
+}
