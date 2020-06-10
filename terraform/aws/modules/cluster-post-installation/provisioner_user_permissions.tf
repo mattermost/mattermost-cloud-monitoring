82c293a174dafa2a26112cde04d15a9d249e63d1
Stylianos Rigas <stylianosrigas@gmail.com>
2020-06-10 17:19:03 +0300

    Add iam:ListAccountAliases permission

--- terraform/aws/modules/cluster-post-installation/provisioner_user_permissions.tf
+++ terraform/aws/modules/cluster-post-installation/provisioner_user_permissions.tf
@@ -283,7 +283,8 @@ resource "aws_iam_policy" "iam" {
             "Action": [
                 "iam:ListRoles",
                 "iam:ListInstanceProfiles",
-                "iam:ListRolePolicies"
+                "iam:ListRolePolicies",
+                "iam:ListAccountAliases"
             ],
             "Resource": "*"
         },
