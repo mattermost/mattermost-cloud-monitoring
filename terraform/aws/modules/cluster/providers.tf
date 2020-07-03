88997d414d4620a8e4974885ecc1ebb1170ac9bb
Angelos Kyratzakos <angelos.kyratzakos@mattermost.com>
2020-07-03 12:04:27 +0300

    comment config path

--- terraform/aws/modules/cluster/providers.tf
+++ terraform/aws/modules/cluster/providers.tf
@@ -9,5 +9,5 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster_auth.token
   load_config_file       = false
-  config_path            = "${var.kubeconfig_dir}/kubeconfig"
+  # config_path            = "${var.kubeconfig_dir}/kubeconfig"
 }
