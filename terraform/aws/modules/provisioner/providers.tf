f37e79eec7fb472c653119260001afb771f5b7f1
Angelos Kyratzakos <angelos.kyratzakos@mattermost.com>
2020-07-03 17:27:59 +0300

    comment path for kubeconfig on providers

--- terraform/aws/modules/provisioner/providers.tf
+++ terraform/aws/modules/provisioner/providers.tf
@@ -3,7 +3,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster_auth.token
   load_config_file       = false
-  config_path            = "${var.kubeconfig_dir}/kubeconfig"
+  # config_path            = "${var.kubeconfig_dir}/kubeconfig"
 }
 
 provider "helm" {
@@ -12,7 +12,7 @@ provider "helm" {
   namespace       = "kube-system"
   tiller_image    = "gcr.io/kubernetes-helm/tiller:v${var.tiller_version}"
   kubernetes {
-    config_path            = "${var.kubeconfig_dir}/kubeconfig"
+    # config_path            = "${var.kubeconfig_dir}/kubeconfig"
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.cluster_auth.token
