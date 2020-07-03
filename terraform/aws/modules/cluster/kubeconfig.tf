6e588c09497eddbf968cc1b8885be646e4c33eab
Angelos Kyratzakos <angelos.kyratzakos@mattermost.com>
2020-07-03 11:29:10 +0300

    remove decoding and mkdir

--- terraform/aws/modules/cluster/kubeconfig.tf
+++ terraform/aws/modules/cluster/kubeconfig.tf
@@ -8,16 +8,16 @@ data "template_file" "kubeconfig" {
   }
 }
 
+# mkdir "${var.kubeconfig_dir}"
 
 resource "null_resource" "cluster_services" {
   provisioner "local-exec" {
     command = <<LOCAL_EXEC
-      mkdir "${var.kubeconfig_dir}"
       touch "${var.kubeconfig_dir}/kubeconfig"
-      echo $KUBECONFIG_DATA | base64 --decode > "${var.kubeconfig_dir}/kubeconfig"
+      echo $KUBECONFIG_DATA > "${var.kubeconfig_dir}/kubeconfig"
     LOCAL_EXEC
     environment = {
-      KUBECONFIG_DATA = "${base64encode(data.template_file.kubeconfig.rendered)}"
+      KUBECONFIG_DATA = "${data.template_file.kubeconfig.rendered}"
     }
   }
 }
