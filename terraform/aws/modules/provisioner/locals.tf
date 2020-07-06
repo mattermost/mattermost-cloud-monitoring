94e44ceb91c24ded084e32b232a569c6ea2f166e
Angelos Kyratzakos <angelos.kyratzakos@mattermost.com>
2020-07-06 15:39:56 +0300

    use provider outside of config

--- terraform/aws/modules/provisioner/locals.tf
+++ terraform/aws/modules/provisioner/locals.tf
@@ -4,11 +4,11 @@ data "aws_region" "current" {}
 data "terraform_remote_state" "cluster" {
   backend = "s3"
   config = {
-    bucket   = "terraform-cloud-monitoring-state-bucket-${var.environment}"
-    key      = "${data.aws_region.current.name}/mattermost-central-command-control"
-    region   = "us-east-1"
-    provider = aws.data
+    bucket = "terraform-cloud-monitoring-state-bucket-${var.environment}"
+    key    = "${data.aws_region.current.name}/mattermost-central-command-control"
+    region = "us-east-1"
   }
+  provider = aws.data
 }
 
 
