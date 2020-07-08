741aef36e39d2e451450ac481cdd22defc15a3ac
Angelos Kyratzakos <angelos.kyratzakos@mattermost.com>
2020-07-08 13:39:19 +0300

    fmt

--- terraform/aws/modules/cluster/locals.tf
+++ terraform/aws/modules/cluster/locals.tf
@@ -3,6 +3,7 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 locals {
+  extra_auth_config_provider = var.provider_role_arn == "" ? "" : local.provider_auth_config
   provider_auth_config       = <<YAML
 
   - rolearn: "${var.provider_role_arn}"
@@ -10,7 +11,4 @@ locals {
     groups:
       - system:masters
   YAML
-  extra_auth_config_provider = var.provider_role_arn == "" ? "" : local.provider_auth_config
-
-
 }
