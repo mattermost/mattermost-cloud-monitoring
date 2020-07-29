d5dcc17a5906318939b359a7a9b940239ac83c3b
Stylianos Rigas <stylianosrigas@gmail.com>
2020-07-29 17:52:32 +0300

    MM-27288 Adding cloud provisioning node policy

---
+++ terraform/aws/modules/vpc-setup/cluster_nodes_iam.tf
@@ -0,0 +1,60 @@
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+
+resource "aws_iam_policy" "node_policy" {
+  name        = "cloud-provisioning-node-policy"
+  path        = "/"
+  description = "Provisioning custom node policy"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {   
+            "Sid": "AllS3Bucket",
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket",
+                "s3:ListBucketVersions",
+                "s3:CreateBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::cloud-${var.environment}-*"
+            ]
+        },
+        {
+            "Sid": "AllS3Object",
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:GetObjectVersion"
+            ],
+            "Resource": [
+                "arn:aws:s3:::cloud-${var.environment}-*/*"
+            ]
+        },
+        {
+            "Sid": "AllActionsOnTeleportDB",
+            "Effect": "Allow",
+            "Action": "dynamodb:*",
+            "Resource": "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/cloud-${var.environment}-*"
+        },
+        {
+            "Sid": "AllActionsOnTeleportIndexDB",
+            "Effect": "Allow",
+            "Action": "dynamodb:*",
+            "Resource": "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/cloud-${var.environment}-*/index/*"
+        },
+        {
+            "Sid": "AllActionsOnTeleportConfigIndexDB",
+            "Effect": "Allow",
+            "Action": "dynamodb:*",
+            "Resource": "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/cloud-${var.environment}-*/stream/*"
+        }
+    ]
+}
+EOF
+}
