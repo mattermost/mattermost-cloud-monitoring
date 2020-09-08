46abdf1f5d675201366f3a548677f95903befb39
Angelos Kyratzakos <angelos.kyratzakos@mattermost.com>
2020-09-08 13:44:41 +0300

    add depends_on on provider

--- terraform/aws/modules/cluster/providers.tf
+++ terraform/aws/modules/cluster/providers.tf
@@ -8,4 +8,9 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster_auth.token
   load_config_file       = false
   # config_path            = "${var.kubeconfig_dir}/kubeconfig"
+  depends_on = [
+    aws_eks_cluster.cluster,
+    aws_autoscaling_group.worker-asg,
+    aws_iam_role.lambda_role
+  ]
 }
