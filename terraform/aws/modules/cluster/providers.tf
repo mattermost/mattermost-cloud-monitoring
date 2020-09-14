4e3bcf7baf31fbf6dab79b985b6d16c1155fca72
Angelos Kyratzakos <angelos.kyratzakos@mattermost.com>
2020-09-14 14:53:13 +0300

    remove commented blocks

--- terraform/aws/modules/cluster/providers.tf
+++ terraform/aws/modules/cluster/providers.tf
@@ -12,5 +12,4 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster_auth.token
   load_config_file       = false
-  # config_path            = "${var.kubeconfig_dir}/kubeconfig"
 }
