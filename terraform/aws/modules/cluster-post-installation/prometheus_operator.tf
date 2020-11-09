98605b5f3771044bdc50bb07f37dbc446cc0e5fd
Stylianos Rigas <stylianosrigas@gmail.com>
2020-11-09 14:57:24 +0000

    MM-30012 Removing old Prometheus master and client, adding Thanos and Prometheus operator deployment, adding blackbox target discovery microservice, cleaning prometheus dns discovery lambda

--- terraform/aws/modules/cluster-post-installation/prometheus_operator.tf
+++ terraform/aws/modules/cluster-post-installation/prometheus_operator.tf
@@ -1,8 +1,9 @@
 resource "helm_release" "prometheus_operator" {
-  name       = "mattermost-cm-prometheus-operator"
+  name       = "prometheus-operator"
   namespace  = "monitoring"
   repository = data.helm_repository.prometheus_community.metadata.0.name
   chart      = "prometheus-community/kube-prometheus-stack"
+  version    = var.prometheus_operator_chart_version
   values = [
     "${file("../../../../chart-values/prometheus_operator_values.yaml")}"
   ]
