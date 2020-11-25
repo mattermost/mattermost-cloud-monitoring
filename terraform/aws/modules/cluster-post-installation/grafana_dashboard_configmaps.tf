292eebbe379f8223321a99550a65a987ff606343
Stavros Foteinopoulos <stafot@gmail.com>
2020-11-25 12:19:57 +0200

    MM-30522: Add DNS grafana dashboards
    Also changed order to be alphabetical for readability purposes

--- terraform/aws/modules/cluster-post-installation/grafana_dashboard_configmaps.tf
+++ terraform/aws/modules/cluster-post-installation/grafana_dashboard_configmaps.tf
@@ -1,3 +1,17 @@
+resource "kubernetes_config_map" "account_monitoring" {
+  metadata {
+    name      = "accountmonitoring-dashboard"
+    namespace = "monitoring"
+    labels = {
+      cloud_dashboards = "account_monitoring"
+    }
+  }
+
+  data = {
+    "acoount_monitoring.json" = "${file("../../../../grafana-dashboards/account_monitoring.json")}"
+  }
+}
+
 resource "kubernetes_config_map" "account_view" {
   metadata {
     name      = "accountview-dashboard"
@@ -12,36 +26,6 @@ resource "kubernetes_config_map" "account_view" {
   }
 }
 
-resource "kubernetes_config_map" "cluster_view" {
-  metadata {
-    name      = "clusterview-dashboard"
-    namespace = "monitoring"
-    labels = {
-      cloud_dashboards = "cluster_view"
-    }
-  }
-
-  data = {
-    "cluster_view.json" = "${file("../../../../grafana-dashboards/cluster_view.json")}"
-  }
-}
-
-
-resource "kubernetes_config_map" "installation_view" {
-  metadata {
-    name      = "installationview-dashboard"
-    namespace = "monitoring"
-    labels = {
-      cloud_dashboards = "installation_view"
-    }
-  }
-
-  data = {
-    "installation_view.json" = "${file("../../../../grafana-dashboards/installation_view.json")}"
-  }
-}
-
-
 resource "kubernetes_config_map" "alerting" {
   metadata {
     name      = "alerting-dashboard"
@@ -53,34 +37,6 @@ resource "kubernetes_config_map" "alerting" {
 
   data = {
     "alerting.json" = "${file("../../../../grafana-dashboards/alerting.json")}"
-  }
-}
-
-resource "kubernetes_config_map" "uptime" {
-  metadata {
-    name      = "uptimeview-dashboard"
-    namespace = "monitoring"
-    labels = {
-      cloud_dashboards = "uptime_view"
-    }
-  }
-
-  data = {
-    "uptime_view.json" = "${file("../../../../grafana-dashboards/uptime_view.json")}"
-  }
-}
-
-resource "kubernetes_config_map" "account_monitoring" {
-  metadata {
-    name      = "accountmonitoring-dashboard"
-    namespace = "monitoring"
-    labels = {
-      cloud_dashboards = "account_monitoring"
-    }
-  }
-
-  data = {
-    "acoount_monitoring.json" = "${file("../../../../grafana-dashboards/account_monitoring.json")}"
   }
 }
 
@@ -98,6 +54,34 @@ resource "kubernetes_config_map" "aws_rds_overview" {
   }
 }
 
+resource "kubernetes_config_map" "cluster_view" {
+  metadata {
+    name      = "clusterview-dashboard"
+    namespace = "monitoring"
+    labels = {
+      cloud_dashboards = "cluster_view"
+    }
+  }
+
+  data = {
+    "cluster_view.json" = "${file("../../../../grafana-dashboards/cluster_view.json")}"
+  }
+}
+
+resource "kubernetes_config_map" "coredns" {
+  metadata {
+    name      = "coredns-dashboard"
+    namespace = "monitoring"
+    labels = {
+      cloud_dashboards = "coredns"
+    }
+  }
+
+  data = {
+    "coredns.json" = "${file("../../../../grafana-dashboards/coredns.json")}"
+  }
+}
+
 resource "kubernetes_config_map" "go_metrics_per_namespace_installation" {
   metadata {
     name      = "gometricspernamespaceinstallation-dashboard"
@@ -109,6 +93,20 @@ resource "kubernetes_config_map" "go_metrics_per_namespace_installation" {
 
   data = {
     "go_metrics_per_namespace_installation.json" = "${file("../../../../grafana-dashboards/go_metrics_per_namespace_installation.json")}"
+  }
+}
+
+resource "kubernetes_config_map" "installation_view" {
+  metadata {
+    name      = "installationview-dashboard"
+    namespace = "monitoring"
+    labels = {
+      cloud_dashboards = "installation_view"
+    }
+  }
+
+  data = {
+    "installation_view.json" = "${file("../../../../grafana-dashboards/installation_view.json")}"
   }
 }
 
@@ -137,5 +135,33 @@ resource "kubernetes_config_map" "mattermost_performance_monitoring_per_cluster"
 
   data = {
     "mattermost_performance_monitoring_per_cluster.json" = "${file("../../../../grafana-dashboards/mattermost_performance_monitoring_per_cluster.json")}"
+  }
+}
+
+resource "kubernetes_config_map" "skydns" {
+  metadata {
+    name      = "skydns-dashboard"
+    namespace = "monitoring"
+    labels = {
+      cloud_dashboards = "skydns"
+    }
+  }
+
+  data = {
+    "coredns.json" = "${file("../../../../grafana-dashboards/skydns.json")}"
+  }
+}
+
+resource "kubernetes_config_map" "uptime" {
+  metadata {
+    name      = "uptimeview-dashboard"
+    namespace = "monitoring"
+    labels = {
+      cloud_dashboards = "uptime_view"
+    }
+  }
+
+  data = {
+    "uptime_view.json" = "${file("../../../../grafana-dashboards/uptime_view.json")}"
   }
 }
