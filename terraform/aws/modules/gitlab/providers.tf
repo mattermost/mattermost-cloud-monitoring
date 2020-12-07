d8978db9d1f7e2bc4a7e9a71fed1220ca59331b0
Stylianos Rigas <stylianosrigas@gmail.com>
2020-12-07 10:52:09 +0000

    MM-25815 Running Terraform fmt

--- terraform/aws/modules/gitlab/providers.tf
+++ terraform/aws/modules/gitlab/providers.tf
@@ -20,7 +20,7 @@ provider "kubernetes" {
 }
 
 provider "helm" {
- 
+
   kubernetes {
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
