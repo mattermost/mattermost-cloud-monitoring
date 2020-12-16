8efee2ece11ee0d150fd1fc7427156baff58644f
Stavros Foteinopoulos <stafot@gmail.com>
2020-12-16 17:52:14 +0200

    Fix S3 bucket naming

--- terraform/aws/modules/security/vpc_flow_logs.tf
+++ terraform/aws/modules/security/vpc_flow_logs.tf
@@ -6,7 +6,7 @@ data "aws_kms_alias" "s3" {
 
 resource "aws_flow_log" "cloud_vpc" {
   count                = var.enable_flow_logs ? length(data.aws_vpcs.vpcs.ids) : 0
-  log_destination      = "arn:aws:s3:::mm-cloud-flowlogs-${var.environment}"
+  log_destination      = "arn:aws:s3:::mattermost-vpc-flow-logs-${var.environment}"
   log_destination_type = "s3"
   traffic_type         = "ALL"
   vpc_id               = element(data.aws_vpcs.vpcs.ids[*], count.index)
