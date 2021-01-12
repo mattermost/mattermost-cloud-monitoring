0d923db7af43f60c234bd891fca3a818bfc2ca63
Stavros Foteinopoulos <stafot@gmail.com>
2021-01-12 16:17:11 +0200

    Refactor directories structure for atlantis

--- terraform/aws/modules/gitlab-monitoring/prometheus.tf
+++ terraform/aws/modules/gitlab-monitoring/prometheus.tf
@@ -5,7 +5,7 @@ resource "helm_release" "prometheus_operator" {
   version    = var.prometheus_operator_chart_version
   repository = "https://prometheus-community.github.io/helm-charts"
   values = [
-    file("../../../chart-values/prometheus_operator_values.yaml")
+    file("../../../chart-values/2-monitoring/prometheus_operator_values.yaml")
   ]
   depends_on = [
     kubernetes_namespace.monitoring
