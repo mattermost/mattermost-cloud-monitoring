fa566ad52f6dbdd0fb35f8cadc9af4256bcc0a2e
Stavros Foteinopoulos <stafot@gmail.com>
2021-01-18 17:20:22 +0200

    MM-31294: Terraform module for budgets

---
+++ terraform/aws/modules/billing/budget.tf
@@ -0,0 +1,17 @@
+resource "aws_budgets_budget" "cloud_budget" {
+  name              = "Cloud-${var.environment}-Budget"
+  budget_type       = "COST"
+  limit_amount      = var.amount
+  limit_unit        = var.currency
+  time_period_start = "2021-01-01_00:00"
+  time_unit         = "MONTHLY"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = var.threshold
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = [var.emails]
+    subscriber_sns_topic_arns  = [var.sns_topic_arns]
+  }
+}
---
+++ terraform/aws/modules/billing/variables.tf
@@ -0,0 +1,6 @@
+variable "environment" {}
+variable "amount" {}
+variable "currency" {}
+variable "threshold" {}
+variable "emails" {}
+variable "sns_topic_arns" {}
