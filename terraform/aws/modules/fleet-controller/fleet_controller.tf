33e7264279d11e2fc03119ded34fd8dae2b8f28e
Stylianos Rigas <stylianosrigas@gmail.com>
2021-02-19 09:32:44 +0000

    Adding a variable for fleet controller max users filter

--- terraform/aws/modules/fleet-controller/fleet_controller.tf
+++ terraform/aws/modules/fleet-controller/fleet_controller.tf
@@ -21,7 +21,7 @@ resource "kubernetes_cron_job" "cloud_fleet_controller_cron" {
               name              = "fleet-controller"
               image             = var.fleet_controller_image
               image_pull_policy = "IfNotPresent"
-              args              = ["hibernate", "--unlock", "--server=${var.provisioner_server}", "--thanos-url=${var.thanos_server}", "--group=${var.cws_group}", "--dry-run=false", "--max-users=11"]
+              args              = ["hibernate", "--unlock", "--server=${var.provisioner_server}", "--thanos-url=${var.thanos_server}", "--group=${var.cws_group}", "--dry-run=false", "--max-users=${var.fleet_controller_max_users_filter}"]
               env {
                 name  = "FC_PRODUCTION_LOGS"
                 value = "true"
