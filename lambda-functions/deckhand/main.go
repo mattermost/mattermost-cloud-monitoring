2bd4b74ec6d2bc5daee5cf2fb6f1ae4c173f9add
Stavros Foteinopoulos <stafot@gmail.com>
2021-02-26 11:24:56 +0200

    MM-32486: Deckhand fixes (#262)

--- lambda-functions/deckhand/main.go
+++ lambda-functions/deckhand/main.go
@@ -6,6 +6,10 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	log "github.com/sirupsen/logrus"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
