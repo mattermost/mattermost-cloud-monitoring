ff5eeb39b0a2f03867bd847238a0f19e02aa9967
Angelos Kyratzakos <angelos.kyratzakos@mattermost.com>
2021-03-02 12:23:27 +0200

    Update kibana cidrs with new vpn cidr (#269)

--- terraform/aws/modules/elasticsearch/security_group.tf
+++ terraform/aws/modules/elasticsearch/security_group.tf
@@ -8,14 +8,14 @@ resource "aws_security_group" "es_sg" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = var.mattermost_network
+    cidr_blocks = concat(var.vpn_cidr, var.mattermost_network)
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = var.mattermost_network
+    cidr_blocks = concat(var.vpn_cidr, var.mattermost_network)
   }
 
   egress {
