70d30cc29e843b1124df8ed10542c0430e375f64
Angelos Kyratzakos <angelos.kyratzakos@mattermost.com>
2021-03-24 21:10:41 +0200

    add permissions for provisioner to create launch templates

--- terraform/aws/modules/provisioner-users/provisioner_user_permissions.tf
+++ terraform/aws/modules/provisioner-users/provisioner_user_permissions.tf
@@ -204,6 +204,7 @@ resource "aws_iam_policy" "ec2" {
                 "ec2:StopInstances",
                 "ec2:DescribeSecurityGroups",
                 "ec2:CreateVolume",
+                "ec2:CreateLaunchTemplate",
                 "ec2:DescribeImages",
                 "ec2:DescribeVpcs",
                 "ec2:AttachNetworkInterface",
