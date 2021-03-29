5dc707ef140f10303d73a0a6a6866799d69fdae7
Stylianos Rigas <stylianosrigas@gmail.com>
2021-03-29 09:02:39 +0100

    MM-34278 Adding descriptions for module variables

--- terraform/aws/modules/genesis-support/variables.tf
+++ terraform/aws/modules/genesis-support/variables.tf
@@ -1,11 +1,23 @@
-variable "share_name_test" {}
+variable "share_name_test" {
+    description = "The AWS share name to associate test Transit Gateways with."
+}
 
-variable "share_name_prod" {}
+variable "share_name_prod" {
+    description = "The AWS share name to associate test Transit Gateways with."
+}
 
-variable "cloud_enterprise_test_tgw" {}
+variable "cloud_enterprise_test_tgw" {
+    description = "The Cloud enterprise test Transit Gateway."
+}
 
-variable "cloud_enterprise_prod_tgw" {}
+variable "cloud_enterprise_prod_tgw" {
+    description = "The Cloud enterprise prod Transit Gateway."
+}
 
-variable "test_account_id" {}
+variable "test_account_id" {
+    description = "The Cloud Test account ID."
+}
 
-variable "prod_account_id" {}
+variable "prod_account_id" {
+    description = "The Cloud Prod account ID."
+}
