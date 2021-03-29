8303b4bf3d143a90eaf35243cf9020a3ed0ff833
Stylianos Rigas <stylianosrigas@gmail.com>
2021-03-29 09:03:32 +0100

    MM-34278 Run Terraform fmt

--- terraform/aws/modules/genesis-support/variables.tf
+++ terraform/aws/modules/genesis-support/variables.tf
@@ -1,23 +1,23 @@
 variable "share_name_test" {
-    description = "The AWS share name to associate test Transit Gateways with."
+  description = "The AWS share name to associate test Transit Gateways with."
 }
 
 variable "share_name_prod" {
-    description = "The AWS share name to associate test Transit Gateways with."
+  description = "The AWS share name to associate test Transit Gateways with."
 }
 
 variable "cloud_enterprise_test_tgw" {
-    description = "The Cloud enterprise test Transit Gateway."
+  description = "The Cloud enterprise test Transit Gateway."
 }
 
 variable "cloud_enterprise_prod_tgw" {
-    description = "The Cloud enterprise prod Transit Gateway."
+  description = "The Cloud enterprise prod Transit Gateway."
 }
 
 variable "test_account_id" {
-    description = "The Cloud Test account ID."
+  description = "The Cloud Test account ID."
 }
 
 variable "prod_account_id" {
-    description = "The Cloud Prod account ID."
+  description = "The Cloud Prod account ID."
 }
