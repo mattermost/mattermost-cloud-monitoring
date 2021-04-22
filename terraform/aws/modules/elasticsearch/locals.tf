032e31fbab91889a53469df61b0de389b9297a92
Spiros Economakis <spiros.oikonomakis@gmail.com>
2021-04-22 14:43:56 +0300

    Fix local for sns

--- terraform/aws/modules/elasticsearch/locals.tf
+++ terraform/aws/modules/elasticsearch/locals.tf
@@ -7,5 +7,5 @@ locals {
     MasterCPUUtilizationThreshold    = min(max(coalesce(var.master_cpu_utilization_threshold, var.cpu_utilization_threshold), 0), 100)
     MasterJVMMemoryPressureThreshold = min(max(coalesce(var.master_jvm_memory_pressure_threshold, var.jvm_memory_pressure_threshold), 0), 100)
   }
-  aws_sns_topic_arn = ["arn:aws:sns:us-east-1:${data.aws_caller_identity.current.account_id}:elb-alarm-topic"]
+  aws_sns_topic_arn = "arn:aws:sns:us-east-1:${data.aws_caller_identity.current.account_id}:elb-alarm-topic"
 }
