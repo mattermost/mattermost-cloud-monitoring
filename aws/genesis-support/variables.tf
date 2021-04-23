85d40d10682cc779c31bcb3a44027409d9cca109
Spiros Economakis <spiros.oikonomakis@gmail.com>
2021-04-23 15:47:08 +0300

    more modules

--- aws/genesis-support/variables.tf
+++ aws/genesis-support/variables.tf
@@ -1,23 +1,29 @@
 variable "share_name_test" {
+  type        = string
   description = "The AWS share name to associate test Transit Gateways with."
 }
 
 variable "share_name_prod" {
+  type        = string
   description = "The AWS share name to associate test Transit Gateways with."
 }
 
 variable "cloud_enterprise_test_tgw" {
-  description = "The Cloud enterprise test Transit Gateway."
+  type        = string
+  description = "The Cloud enterprise test Transit Gateway ID."
 }
 
 variable "cloud_enterprise_prod_tgw" {
-  description = "The Cloud enterprise prod Transit Gateway."
+  type        = string
+  description = "The Cloud enterprise prod Transit Gateway ID."
 }
 
 variable "test_account_id" {
+  type        = string
   description = "The Cloud Test account ID."
 }
 
 variable "prod_account_id" {
+  type        = string
   description = "The Cloud Prod account ID."
 }
