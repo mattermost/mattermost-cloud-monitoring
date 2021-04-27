75a8af1c573d5ef7071bdf620cdb83c0f3548a2a
Spiros Economakis <spiros.oikonomakis@gmail.com>
2021-04-27 21:33:49 +0300

    Change fluentbit helm to the official chart (#300)
    We are bumping the version of fluentbit and we need to source the new
    official helm chart.
    
    https://mattermost.atlassian.net/browse/MM=35208

--- terraform/aws/modules/cluster-post-installation/fluent-bit.tf
+++ terraform/aws/modules/cluster-post-installation/fluent-bit.tf
@@ -1,7 +1,7 @@
 resource "helm_release" "fluent_bit" {
   name       = "mattermost-cm-fluent-bit"
   namespace  = "logging"
-  repository = "https://charts.helm.sh/stable"
+  repository = "https://fluent.github.io/helm-charts"
   chart      = "fluent-bit"
   version    = var.fluent_bit_chart_version
   values = [
