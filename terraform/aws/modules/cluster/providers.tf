7d2374e68e94f86b6d8a1bea9856c3c5e9b7018a
Stavros Foteinopoulos <stafot@gmail.com>
2021-05-15 20:09:49 +0300

    Pass AWS_PROFILE as param
    If this commit applied aws profile could pass as param on cluster module when it get's token. This way can be ensured that it will use the profile which had the proper permissions and not on inherited from EC2 instance role.
    Issue: MM-35549

--- terraform/aws/modules/cluster/providers.tf
+++ terraform/aws/modules/cluster/providers.tf
@@ -3,7 +3,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority.0.data)
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
-    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.cluster.name]
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.cluster.name, "--profile", var.aws_profile]
     command     = "aws"
   }
 }
