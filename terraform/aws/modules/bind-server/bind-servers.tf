1fda5e5811d68cdaeab6fe6753d62a669d700847
Stavros Foteinopoulos <stafot@gmail.com>
2021-06-10 15:12:35 +0300

    Add volume size & type as variables to bind servers (#318)
    If this commit applied bind server could have different volume size based on the needs of each use-case.
    Issue: MM-36079

--- terraform/aws/modules/bind-server/bind-servers.tf
+++ terraform/aws/modules/bind-server/bind-servers.tf
@@ -53,12 +53,19 @@ resource "aws_key_pair" "bind" {
   public_key = var.ssh_key_public
 }
 
-resource "aws_launch_configuration" "bind_lauch_configuration" {
+resource "aws_launch_configuration" "bind_launch_configuration" {
   name_prefix     = "${var.name}-"
   image_id        = var.ami
   instance_type   = var.instance_type
   key_name        = "mattermost-cloud-${var.environment}-bind"
   security_groups = [aws_security_group.bind_sg.id]
+
+  root_block_device {
+    volume_size           = var.volume_size
+    volume_type           = var.volume_type
+    delete_on_termination = true
+  }
+
   lifecycle {
     create_before_destroy = true
   }
@@ -66,7 +73,7 @@ resource "aws_launch_configuration" "bind_lauch_configuration" {
 
 resource "aws_autoscaling_group" "bind_autoscale" {
   name                      = "autoscale-bind-server"
-  launch_configuration      = aws_launch_configuration.bind_lauch_configuration.name
+  launch_configuration      = aws_launch_configuration.bind_launch_configuration.name
   min_size                  = 3
   max_size                  = 3
   desired_capacity          = 3
