1caaf7ed4ff9b49d952d8e0161d3f54664b7ce68
Stylianos Rigas <stylianosrigas@gmail.com>
2021-06-17 16:48:34 +0100

    MM-36471 Adding AWAT infrastructure module

---
+++ terraform/aws/modules/awat/locals.tf
@@ -0,0 +1,20 @@
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+data "terraform_remote_state" "cnc_cluster" {
+  backend = "s3"
+  config = {
+    bucket   = "terraform-cloud-monitoring-state-bucket-${var.environment}"
+    key      = "${data.aws_region.current.name}/mattermost-central-command-control"
+    region   = "us-east-1"
+  }
+}
+
+locals {
+  conditional_dash_region = data.aws_region.current.name == "us-east-1" ? "" : "-${data.aws_region.current.name}"
+  timestamp_now           = formatdate("YYYY-MM-DD-hh-mm", timestamp())
+}
+
+locals {
+  db_identifier_read_replica = "${var.db_identifier}-read"
+}
