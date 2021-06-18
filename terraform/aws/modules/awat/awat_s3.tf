85360cf105c2f3a2c5591c5fd8103eb4a80560ec
Stylianos Rigas <stylianosrigas@gmail.com>
2021-06-18 09:36:15 +0100

    [MM 36471] Adding AWAT infrastructure module (#319)
    * MM-36471 Add route53 record for AWAT service
    
    * MM-36471 Adding AWAT infrastructure module
    
    * MM-36471 Revert Route53 changes
    
    * MM-36471 Terraform fmt

---
+++ terraform/aws/modules/awat/awat_s3.tf
@@ -0,0 +1,82 @@
+resource "aws_s3_bucket" "awat_bucket" {
+  bucket = "cloud-awat-${var.environment}"
+  acl    = "private"
+  policy = var.enable_awat_bucket_restriction ? data.aws_iam_policy_document.awat_bucket_policy.json : ""
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = data.aws_kms_key.master_s3.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  versioning {
+    enabled    = true
+    mfa_delete = false
+  }
+}
+
+data "aws_kms_key" "master_s3" {
+  key_id = "alias/aws/s3"
+}
+
+data "aws_iam_user" "cnc_user" {
+  user_name = var.cnc_user
+}
+
+
+data "aws_iam_policy_document" "awat_bucket_policy" {
+
+  statement {
+    effect = "Deny"
+    actions = [
+      "s3:GetObject*",
+      "s3:DeleteObject*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    resources = [
+      "arn:aws:s3:::cloud-awat-${var.environment}",
+      "arn:aws:s3:::cloud-awat-${var.environment}/*"
+    ]
+
+    condition {
+      test     = "StringNotLike"
+      variable = "aws:userId"
+
+      values = flatten([[
+        aws_iam_user.awat_user.unique_id
+      ], ["AROA*"]])
+    }
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    resources = [
+      "arn:aws:s3:::cloud-awat-${var.environment}",
+      "arn:aws:s3:::cloud-awat-${var.environment}/*"
+    ]
+
+    condition {
+      test     = "StringLike"
+      variable = "aws:userId"
+
+      values = [
+        data.aws_iam_user.cnc_user.user_id
+      ]
+    }
+  }
+
+}
+
---
+++ terraform/aws/modules/awat/variables.tf
@@ -0,0 +1,48 @@
+variable "environment" {}
+
+variable "vpc_id" {}
+
+variable "region" {}
+
+variable "cloud_vpn_cidr" {}
+
+variable "db_identifier" {}
+
+variable "allocated_db_storage" {}
+
+variable "db_engine_version" {}
+
+variable "db_instance_class" {}
+
+variable "db_master_az" {}
+
+variable "db_read_replica_az" {}
+
+variable "db_name" {}
+
+variable "db_username" {}
+
+variable "db_password" {}
+
+variable "db_backup_retention_period" {}
+
+variable "db_backup_window" {}
+
+variable "db_maintenance_window" {}
+
+variable "snapshot_identifier" {}
+
+variable "storage_encrypted" {}
+
+variable "private_subnets" {}
+
+variable "enable_awat_read_replica" {}
+
+variable "db_deletion_protection" {
+  type    = bool
+  default = true
+}
+
+variable "enable_awat_bucket_restriction" {}
+
+variable "cnc_user" {}
