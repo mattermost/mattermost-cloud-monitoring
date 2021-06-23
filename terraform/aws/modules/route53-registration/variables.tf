f98c984bd32b8f1abbfdbcb8df9d8bc2f9d21293
Stylianos Rigas <stylianosrigas@gmail.com>
2021-06-23 15:55:36 +0100

    MM-36620 Add control for AWAT, portal private and private public route53 endpoints (#321)

--- terraform/aws/modules/route53-registration/variables.tf
+++ terraform/aws/modules/route53-registration/variables.tf
@@ -6,6 +6,10 @@ variable "private_hosted_zoneid" {}
 
 variable "public_hosted_zoneid" {}
 
-variable "enable_portal_r53_record" {}
+variable "enable_portal_public_r53_record" {}
+
+variable "enable_portal_private_r53_record" {}
 
 variable "enable_portal_internal_r53_record" {}
+
+variable "enable_awat_record" {}
