a26fe326ca96bf475c99eabcd7a5c859fee8a305
Stylianos Rigas <stylianosrigas@gmail.com>
2021-07-09 11:06:24 +0300

    MM-36075 Run Terraform FMRT

--- terraform/aws/modules/eks-managed-node-groups/variables.tf
+++ terraform/aws/modules/eks-managed-node-groups/variables.tf
@@ -1,59 +1,59 @@
 variable "vpc_security_group_ids" {
-    description = "The IDs of the security groups that will be assigned to the cluster nodes"
+  description = "The IDs of the security groups that will be assigned to the cluster nodes"
 }
 
 variable "volume_size" {
-    description = "The size of the node volumes"
+  description = "The size of the node volumes"
 }
 
 variable "volume_type" {
-    description = "The type of the node volumes. For example gp2"
+  description = "The type of the node volumes. For example gp2"
 }
 
 variable "image_id" {
-    description = "The AMI ID used for the nodes in the node group"
+  description = "The AMI ID used for the nodes in the node group"
 }
 
 variable "instance_type" {
-    description = "The instance type used for the nodes in the node group"
+  description = "The instance type used for the nodes in the node group"
 }
 
 variable "user_data" {
-    description = "User data passed in the launch template to run in instance boot"
+  description = "User data passed in the launch template to run in instance boot"
 }
 
 variable "cluster_name" {
-    description = "The name of the cluster that the node group will be assigned to"
+  description = "The name of the cluster that the node group will be assigned to"
 }
 
 variable "node_role_arn" {
-    description = "The ARN of the IAM role attached to the node group"
+  description = "The ARN of the IAM role attached to the node group"
 }
 
 variable "subnet_ids" {
-    description = "A list of Subnet IDs that nodes will be deployed into"
+  description = "A list of Subnet IDs that nodes will be deployed into"
 }
 
 variable "deployment_name" {
-    description = "A phrase that can be used for tagging tha identifies the deployment"
+  description = "A phrase that can be used for tagging tha identifies the deployment"
 }
 
 variable "desired_size" {
-    description = "The desired number of nodes in the node group"
+  description = "The desired number of nodes in the node group"
 }
 
 variable "max_size" {
-    description = "The maximum number of nodes in the node group"
+  description = "The maximum number of nodes in the node group"
 }
 
 variable "min_size" {
-    description = "The minimum number of nodes in the node group"
+  description = "The minimum number of nodes in the node group"
 }
 
 variable "node_group_name" {
-    description = "A name that can be used to identify the node group"
+  description = "A name that can be used to identify the node group"
 }
 
 variable "cluster_short_name" {
-    description = "A short name that identifies the cluster"
+  description = "A short name that identifies the cluster"
 }
