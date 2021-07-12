f46c099cc8d71236df8eadd90e1dd67f1742ff03
Stylianos Rigas <stylianosrigas@gmail.com>
2021-07-12 15:59:59 +0300

    MM-36896 Add permissions to be able to get EKS information via the console

--- terraform/aws/modules/eks-cluster/aws_auth_configmap.tf
+++ terraform/aws/modules/eks-cluster/aws_auth_configmap.tf
@@ -12,10 +12,60 @@ resource "kubernetes_config_map" "aws_auth_configmap" {
     groups:
       - system:bootstrappers
       - system:nodes
+  - rolearn: arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/AWSReservedSSO_AWSAdministratorAccess_${var.aws_reserved_sso_id}
+    username: system:masters
+    groups:
+      - eks-console-dashboard-full-access-group
+  - rolearn: arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/Cloud${title(var.environment)}Admin
+    username: system:masters
+    groups:
+      - eks-console-dashboard-full-access-group
   YAML
   }
   depends_on = [
     aws_eks_cluster.cluster,
     null_resource.cluster_services
   ]
+}
+
+resource "kubernetes_cluster_role_binding" "console_access" {
+  metadata {
+    name = "eks-console-dashboard-full-access-binding"
+  }
+
+  role_ref {
+    kind      = "ClusterRole"
+    name      = "eks-console-dashboard-full-access-clusterrole"
+    api_group = "rbac.authorization.k8s.io"
+  }
+
+  subject {
+    kind = "Group"
+    name = "eks-console-dashboard-full-access-group"
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
+
+resource "kubernetes_cluster_role" "console_access" {
+  metadata {
+    name = "eks-console-dashboard-full-access-clusterrole"
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["nodes", "namespaces", "pods"]
+    verbs      = ["get", "list"]
+  }
+
+  rule {
+    api_groups = ["apps"]
+    resources  = ["deployments", "daemonsets", "statefulsets", "replicasets"]
+    verbs      = ["get", "list"]
+  }
+
+  rule {
+    api_groups = ["batch"]
+    resources  = ["jobs"]
+    verbs      = ["get", "list"]
+  }
 }
