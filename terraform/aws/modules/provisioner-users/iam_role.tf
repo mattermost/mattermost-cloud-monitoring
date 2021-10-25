fa434a3e8c0d64540016259b9b3d70f39cb9eefa
Spiros Economakis <spiros.oikonomakis@gmail.com>
2021-10-25 10:17:44 +0300

    Fix reference iam role

--- terraform/aws/modules/provisioner-users/iam_role.tf
+++ terraform/aws/modules/provisioner-users/iam_role.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "provisioner_role" {
-  name = "k8s-${var.environment}-provisioner-role"
+  name = "k8s-${var.environment}-provisioner_role"
 
   assume_role_policy = <<EOF
 {
@@ -18,51 +18,51 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "provisioner_role_attach_route53" {
-  role       = aws_iam_role.provisioner-role.name
+  role       = aws_iam_role.provisioner_role.name
   policy_arn = aws_iam_policy.route53.arn
 }
 
 resource "aws_iam_role_policy_attachment" "provisioner_role_attach_rds" {
-  role       = aws_iam_role.provisioner-role.name
+  role       = aws_iam_role.provisioner_role.name
   policy_arn = aws_iam_policy.rds.arn
 }
 
 resource "aws_iam_role_policy_attachment" "provisioner_role_attach_s3" {
-  role       = aws_iam_role.provisioner-role.name
+  role       = aws_iam_role.provisioner_role.name
   policy_arn = aws_iam_policy.s3.arn
 }
 
 resource "aws_iam_role_policy_attachment" "provisioner_role_attach_secrets_manager" {
-  role       = aws_iam_role.provisioner-role.name
+  role       = aws_iam_role.provisioner_role.name
   policy_arn = aws_iam_policy.secrets_manager.arn
 }
 
 resource "aws_iam_role_policy_attachment" "provisioner_role_attach_ec2" {
-  role       = aws_iam_role.provisioner-role.name
+  role       = aws_iam_role.provisioner_role.name
   policy_arn = aws_iam_policy.ec2.arn
 }
 
 resource "aws_iam_role_policy_attachment" "provisioner_role_attach_vpc" {
-  role       = aws_iam_role.provisioner-role.name
+  role       = aws_iam_role.provisioner_role.name
   policy_arn = aws_iam_policy.vpc.arn
 }
 
 resource "aws_iam_role_policy_attachment" "provisioner_role_attach_iam" {
-  role       = aws_iam_role.provisioner-role.name
+  role       = aws_iam_role.provisioner_role.name
   policy_arn = aws_iam_policy.iam.arn
 }
 
 resource "aws_iam_role_policy_attachment" "provisioner_role_attach_kms" {
-  role       = aws_iam_role.provisioner-role.name
+  role       = aws_iam_role.provisioner_role.name
   policy_arn = aws_iam_policy.kms.arn
 }
 
 resource "aws_iam_role_policy_attachment" "provisioner_role_attach_tag" {
-  role       = aws_iam_role.provisioner-role.name
+  role       = aws_iam_role.provisioner_role.name
   policy_arn = aws_iam_policy.tag.arn
 }
 
 resource "aws_iam_role_policy_attachment" "provisioner_role_attach_dynamodb" {
-  role       = aws_iam_role.provisioner-role.name
+  role       = aws_iam_role.provisioner_role.name
   policy_arn = aws_iam_policy.dynamodb.arn
 }
