b274c674d63ac79618e5cf948418ece48935a49e
Spiros Economakis <spiros.oikonomakis@gmail.com>
2021-10-27 16:19:12 +0300

    [provisioner-users] Add a new IAM role to be used in kube2iam (#357)
    We transitioned from AWS API keys to kube2iam and IAM roles. This commit introduces a new role
    which includes every permission provisioner needs to be functional.
    
    Ticket: https://mattermost.atlassian.net/browse/MM-38616

--- terraform/aws/modules/provisioner-users/variables.tf
+++ terraform/aws/modules/provisioner-users/variables.tf
@@ -1,15 +1,31 @@
-variable "environment" {}
+variable "environment" {
+  type        = string
+  description = "The name of the environment is created"
+}
 
-variable "provisioner_users" {}
+variable "deployment_name" {
+  type        = string
+  description = "The name of the deployment which exists in"
+}
 
-variable "cnc_user" {}
+variable "provisioner_users" {
+  type        = list(string)
+  description = "The list defined the users can use provisioner"
+}
+
+variable "cnc_user" {
+  type        = string
+  description = "The Command And Control user"
+}
 
 variable "force_destroy_state_bucket" {
-  type    = bool
-  default = false
+  type        = bool
+  description = "Enables force destroy of a provisioner state bucket"
+  default     = false
 }
 
 variable "force_destroy_stackrox_bundle_bucket" {
-  type    = bool
-  default = false
+  type        = bool
+  description = "Enables force destroy of a stackrox bundle bucket"
+  default     = false
 }
