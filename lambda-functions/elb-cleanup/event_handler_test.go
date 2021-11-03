53258f67b16b59580721de0c2bea82b5c1f0a3b9
Muhammad Shahid <muhammadshahidtag@gmail.com>
2021-11-03 19:05:56 +1100

    MM-38981 test mock package

--- lambda-functions/elb-cleanup/event_handler_test.go
+++ lambda-functions/elb-cleanup/event_handler_test.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	// "github.com/mattermost/mattermost-cloud-monitoring/lambda-functions/elb-cleanup/mocks"
+	"github.com/mattermost/mattermost-cloud-monitoring/MM-38981-cleanup-unused-LBs/lambda-functions/elb-cleanup/mocks"
+	"github.com/sirupsen/logrus"
+)
+
+func TestHandle(t *testing.T) {
+	gmctrl := gomock.NewController(t)
+	awsResourcer := mocks.NewMockResourcer(gmctrl)
+	eventHandler := NewEventHandler(90, awsResourcer, false, logrus.New())
+	fmt.Println(eventHandler.expirationDays)
+}
