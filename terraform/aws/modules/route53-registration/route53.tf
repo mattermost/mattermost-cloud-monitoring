a478307ae3cd0ea2170e340b7019ce8285265433
Stylianos Rigas <stylianosrigas@gmail.com>
2021-11-04 16:22:49 +0200

    MM-39183 Migrate push proxy record to be private (#360)

--- terraform/aws/modules/route53-registration/route53.tf
+++ terraform/aws/modules/route53-registration/route53.tf
@@ -152,9 +152,9 @@ resource "aws_route53_record" "kubecost" {
 resource "aws_route53_record" "push_proxy" {
   count = var.enable_push_proxy_record ? 1 : 0
 
-  zone_id = var.public_hosted_zoneid
+  zone_id = var.private_hosted_zoneid
   name    = "push"
   type    = "CNAME"
   ttl     = "60"
-  records = [data.kubernetes_service.nginx-public.status.0.load_balancer.0.ingress.0.hostname]
+  records = [data.kubernetes_service.nginx-private.status.0.load_balancer.0.ingress.0.hostname]
 }
