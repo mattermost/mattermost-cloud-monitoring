7b85cdae37c42cd3469045064a0909ae6a726d28
Spiros Economakis <spiros.oikonomakis@gmail.com>
2021-11-08 17:58:24 +0200

    Add module for gitlab runner for terraform (#361)
    Module for creation of a bucket about gitlab runners and the required IAM role.
    
    Ticket: https://mattermost.atlassian.net/browse/MM-38616

---
+++ terraform/aws/modules/gitlab-runner/main.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "gitlab_runners" {
+  bucket = "mattermost-cloud-gitlab-runners"
+  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = data.aws_kms_key.master_s3.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  versioning {
+    enabled    = true
+    mfa_delete = false
+  }
+}
