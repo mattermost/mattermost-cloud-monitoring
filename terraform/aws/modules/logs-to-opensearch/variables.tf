07f4251f859b5afde5c417f8a111f112965fcc0e
Stavros Foteinopoulos <stafot@gmail.com>
2021-11-09 10:24:51 +0200

    Add lambda to ship Cloudwatch logs to Opensearch (#359)
    * Add lambda to ship Cloudwatch logs to Opensearch
    
    Issue: MM-39482
    
    * Review fixes
    
    Issue:
    
    * Apply suggestions from code review
    
    Co-authored-by: Kyriakos Z. <3829551+koox00@users.noreply.github.com>
    
    * Update lambda-functions/logs-to-opensearch/index.js
    
    Co-authored-by: Kyriakos Z. <3829551+koox00@users.noreply.github.com>
    
    Co-authored-by: Kyriakos Z. <3829551+koox00@users.noreply.github.com>

---
+++ terraform/aws/modules/logs-to-opensearch/variables.tf
@@ -0,0 +1,46 @@
+variable "private_subnet_ids" {
+  type        = list(string)
+  description = "The list of the private subnet IDs are used by logs-to-opensearch lambda"
+}
+variable "vpc_id" {
+  description = "The ID of VPC is used by the logs-to-opensearch lamda"
+  type = string
+}
+
+variable "deployment_name" {
+  description = "The name of the deployment for Lambda"
+  type = string
+}
+
+variable "region" {
+  description = "The region which will be used in logs-to-opensearch lamda"
+  type = string
+}
+
+variable "bucket" {
+  description = "S3 bucket where the logs-to-opensearch lambda is stored"
+  type = string
+}
+
+variable "es_endpoint" {
+  description = "The endpoint of AWS Opensearch service"
+  type = string
+}
+
+variable "alarm_period" {
+  default = 10800
+  description = "The period in seconds over which the specified statistic is applied"
+  type = number
+}
+
+variable "alarm_evaluation_periods" {
+  default = 1
+  description = "The number of periods over which data is compared to the specified threshold"
+  type = number
+}
+
+variable "alarm_threshold" {
+  default = 1
+  description = "The value against which the specified statistic is compared"
+  type = number
+}
