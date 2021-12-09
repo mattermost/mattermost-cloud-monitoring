9363ba7ccc5d9a5757412feca3875c2e9c4d1dcf
Stylianos Rigas <stylianosrigas@gmail.com>
2021-12-09 15:05:07 +0200

    MM-40224 Run Terraform fmt

--- terraform/aws/modules/loki/variables.tf
+++ terraform/aws/modules/loki/variables.tf
@@ -1,14 +1,14 @@
 variable "enable_loki_bucket_restriction" {
-    type = bool
-    description = "Whether to enable Loki bucket policy or not"
+  type        = bool
+  description = "Whether to enable Loki bucket policy or not"
 }
 
 variable "cnc_user" {
-    type = string
-    description = "The command and control IAM user"
+  type        = string
+  description = "The command and control IAM user"
 }
 
 variable "environment" {
-    type = string
-    description = "The cloud environment, dev, test, staging or prod."
+  type        = string
+  description = "The cloud environment, dev, test, staging or prod."
 }
