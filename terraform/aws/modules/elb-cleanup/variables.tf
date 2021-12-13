eae01aebb89adde99384db75f4019b8cc550ca67
Muhammad Shahid <muhammadshahidtag@gmail.com>
2021-12-14 09:57:37 +1100

    MM-39689 fixed terraform formating

--- terraform/aws/modules/elb-cleanup/variables.tf
+++ terraform/aws/modules/elb-cleanup/variables.tf
@@ -2,6 +2,7 @@ variable "private_subnet_ids" {
   type        = list(string)
   description = "The list of the private subnet IDs are used by elb-cleanup lambda"
 }
+
 variable "vpc_id" {
   description = "The ID of VPC is used by the elb-cleanup lamda"
   type        = string
@@ -12,21 +13,19 @@ variable "deployment_name" {
   type        = string
 }
 
-variable "elb_cleanup_lambda_schedule" {
- default =  "rate(7 days)"
- description = "A rate expression starts when you create the scheduled event rule, and then runs on its defined schedule."
- type        = string
-}
-
 variable "bucket" {
   description = "S3 bucket where the elb-cleanup lambda is stored"
   type        = string
 }
-variable "dryrun" {
- default = "true"
- description = "S3 bucket where the elb-cleanup lambda is stored"
- type        = string
+
+variable "elb_cleanup_lambda_schedule" {
+  default     = "rate(7 days)"
+  description = "A rate expression and then runs on its defined schedule."
+  type        = string
 }
 
-
-
+variable "dryrun" {
+  default     = "true"
+  description = "S3 bucket where the elb-cleanup lambda is stored"
+  type        = string
+}
