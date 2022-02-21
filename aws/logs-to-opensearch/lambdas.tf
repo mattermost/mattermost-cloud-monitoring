3142d20fd331424e94a4fb9197836afb80b28561
Spiros Economakis <spiros.oikonomakis@gmail.com>
2022-02-21 12:32:24 +0200

    add missing terraform module logs-to-opensearch

---
+++ aws/logs-to-opensearch/lambdas.tf
@@ -0,0 +1,40 @@
+# Lambda function for shipping cloudwatch logs to Opensearch
+resource "aws_lambda_function" "logs_to_opensearch" {
+  s3_bucket     = var.bucket
+  s3_key        = "mattermost-cloud/logs-to-opensearch/master/logs-to-opensearch.zip"
+  function_name = "logs-to-opensearch"
+  description   = "Lambda"
+  role          = aws_iam_role.logs_to_opensearch.arn
+  handler       = "index.handler"
+  runtime       = "nodejs14.x"
+  timeout       = "600"
+
+
+  environment {
+    variables = {
+      "es_endpoint" = var.es_endpoint
+    }
+  }
+
+  vpc_config {
+    subnet_ids         = flatten(var.private_subnet_ids)
+    security_group_ids = [aws_security_group.logs_to_opensearch_sg.id]
+  }
+
+}
+resource "aws_security_group" "logs_to_opensearch_sg" {
+  name        = "${var.deployment_name}-logs-to-opensearch-sg"
+  description = "Ship Cloudwatch Logs to Opensearch"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.deployment_name}-logs-to-opensearch-sg"
+  }
+}
