5e3390afa701a0c42a0fa2430741ad228ccbe6c7
Muhammad Shahid <muhammadshahidtag@gmail.com>
2022-04-01 17:17:32 +1100

    MM-42367 added description for the variables

--- aws/community-infra/variables.tf
+++ aws/community-infra/variables.tf
@@ -1,16 +1,32 @@
-variable "environment" {}
+variable "environment" {
+  type        = string
+  description = "The environment name like staging, staging-dr, prod etc where we want to setup resources"
+}
 
-variable "deployment_name" {}
+variable "deployment_name" {
+  type        = string
+  description = "The name of the deployment"
+}
 
-variable "vpc_id" {}
+variable "vpc_id" {
+  type        = string
+  description = "The vpc id which host the cluster & access the bucket"
+}
 
-variable "destination_bucket" {}
+variable "destination_bucket" {
+  type        = string
+  description = "The ARN of the destination bucket for cross region replication"
+}
 
-variable "destination_s3_kms_key" {}
+variable "destination_s3_kms_key" {
+  type        = string
+  description = "The destination SSE KMS key to encrypt/decrypt the bucket data"
+}
 
 variable "s3_cross_region_replication_enabled" {
-  type    = bool
-  default = true
+  type        = bool
+  default     = true
+  description = "Cross region replication flag for the bucket"
 }
 
 
