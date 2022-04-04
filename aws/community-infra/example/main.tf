dba37e7d17621bc046700b4daed8fb5a8a17f7da
Muhammad Shahid <muhammadshahidtag@gmail.com>
2022-04-04 17:20:57 +1000

    MM-42367 added an example & fix the variable case

---
+++ aws/community-infra/example/main.tf
@@ -0,0 +1,14 @@
+module "community-dr-infra" {
+   source      = "git::https://github.com/mattermost/mattermost-cloud-monitoring.git//aws/community-infra?ref=v1.0.0"
+   environment = "Testing"
+   vpc_id      = var.vpc_id
+   deployment_name = var.deployment_name
+   destination_bucket = var.destination_bucket
+   s3_cross_region_replication_enabled      = true
+   destination_s3_kms_key= var.destination_s3_kms_key
+   tags = {
+      "Owner"	      = "my-team",
+      "Terraform"   = "true",
+      "Purpose"     = "provisioning"
+   }
+}
