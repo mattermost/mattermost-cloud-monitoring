97397be36f1faed340699cc13f678d1b2424cbef
Stylianos Rigas <stylianosrigas@gmail.com>
2022-05-11 16:54:47 +0300

    MM-44077 Add module for 4az subnet VPCs

---
+++ aws/subnet-and-networking-4az/provisioner_db_subnet_group.tf
@@ -0,0 +1,41 @@
+resource "aws_db_subnet_group" "provisioner_db_subnet_group" {
+
+  for_each = toset(var.vpc_cidrs)
+
+  name = "mattermost-provisioner-db-${data.aws_vpc.vpc_ids[each.value]["id"]}"
+
+  subnet_ids = [
+    aws_subnet.private_1a[each.value]["id"],
+    aws_subnet.private_1b[each.value]["id"],
+    aws_subnet.private_1c[each.value]["id"],
+    aws_subnet.private_1d[each.value]["id"]
+  ]
+
+  tags = merge(
+    {
+      "MattermostCloudInstallationDatabase" = "MYSQL/Aurora"
+    },
+    var.tags
+  )
+}
+
+resource "aws_db_subnet_group" "provisioner_db_subnet_group_postgresql" {
+
+  for_each = toset(var.vpc_cidrs)
+
+  name = "mattermost-provisioner-db-${data.aws_vpc.vpc_ids[each.value]["id"]}-postgresql"
+
+  subnet_ids = [
+    aws_subnet.private_1a[each.value]["id"],
+    aws_subnet.private_1b[each.value]["id"],
+    aws_subnet.private_1c[each.value]["id"],
+    aws_subnet.private_1d[each.value]["id"]
+  ]
+
+  tags = merge(
+    {
+      "MattermostCloudInstallationDatabase" = "PostgreSQL/Aurora"
+    },
+    var.tags
+  )
+}
