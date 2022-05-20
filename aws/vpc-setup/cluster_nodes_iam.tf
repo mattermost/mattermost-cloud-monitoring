8064b8f0e572bbae256d15c5c54eeeffd5dc5234
Stylianos Rigas <stylianosrigas@gmail.com>
2022-05-20 14:17:44 +0300

    MM-44459 Add node policy for Thanos sidecar s3 access to metrics bucket

--- aws/vpc-setup/cluster_nodes_iam.tf
+++ aws/vpc-setup/cluster_nodes_iam.tf
@@ -23,6 +23,31 @@ resource "aws_iam_policy" "node_policy" {
     "Version": "2012-10-17",
     "Statement": [
         {
+            "Sid": "AllS3Bucket",
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket",
+                "s3:ListBucketVersions",
+                "s3:CreateBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::cloud-${var.environment}-*"
+            ]
+        },
+        {
+            "Sid": "AllS3Object",
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:GetObjectVersion",
+                "s3:DeleteObject*"
+            ],
+            "Resource": [
+                "arn:aws:s3:::cloud-${var.environment}-*/*"
+            ]
+        },
+        {
             "Action": [
                 "autoscaling:DescribeAutoScalingInstances"
             ],
