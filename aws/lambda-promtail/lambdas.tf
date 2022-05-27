c28c847a78faeb94a3008d85cbbe3fdf133497c8
Stavros Foteinopoulos <stafot@gmail.com>
2022-05-27 16:10:44 +0300

    Minor changes to lambda-promtail (#412)
    Issue: CLD-2297

--- aws/lambda-promtail/lambdas.tf
+++ aws/lambda-promtail/lambdas.tf
@@ -10,8 +10,8 @@ resource "aws_lambda_function" "lambda_promtail" {
   # From the Terraform AWS Lambda docs: If both subnet_ids and security_group_ids are empty then vpc_config is considered to be empty or unset.
   vpc_config {
     # Every subnet should be able to reach an EFS mount target in the same Availability Zone. Cross-AZ mounts are not permitted.
-    subnet_ids         = var.lambda_vpc_subnets
-    security_group_ids = var.lambda_vpc_security_groups
+    subnet_ids         = flatten(var.private_subnet_ids)
+    security_group_ids = [aws_security_group.lambda_promtail_sg.id]
   }
 
   environment {
@@ -35,4 +35,21 @@ resource "aws_lambda_function" "lambda_promtail" {
 resource "aws_lambda_function_event_invoke_config" "lambda_promtail_invoke_config" {
   function_name          = aws_lambda_function.lambda_promtail.function_name
   maximum_retry_attempts = 2
+}
+
+resource "aws_security_group" "lambda_promtail_sg" {
+  name        = "${var.deployment_name}-lambda-promtail-sg"
+  description = "Promtail Lambda"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.deployment_name}-lambda-promtail-sg"
+  }
 }
