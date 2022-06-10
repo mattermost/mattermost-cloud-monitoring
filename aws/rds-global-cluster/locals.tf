f551b02243859d938b8e65b687ea86f96af742e3
Muhammad Shahid <53712140+Mshahidtaj@users.noreply.github.com>
2022-06-10 16:42:24 +1000

    CLD-682 Global RDS cluster module (#414)
    * CLD-682 Added a new module to create/manage global RDS cluster
    
    * CLD-682 Added dependency for secondary cluster , would be help full for deletion
    
    * CLD-682 fixed formating
    
    * CLD-682 added changes like dynamic naming for final backup & other formating fixes
    
    * CLD-682 removed final_snapshot_identifier variable as it is generated now
    
    * CLD-682 set default null value for database name
    
    * CLD-682 made the db name mandatory
    
    * CLD-682 made the db name mandatory
    
    * CLD-682 fixed the dependencies between primary & secondary clusters & dynamic name for final snapshot
    
    * CLD-682 fixed formating
    
    Co-authored-by: Muhammad Shahid <muhammadshahidtag@gmail.com>

---
+++ aws/rds-global-cluster/locals.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  alias  = "primary"
+  region = local.primary.region
+}
+
+provider "aws" {
+  alias  = "secondary"
+  region = local.secondary.region
+}
+
+locals {
+  primary = {
+    region = var.source_region
+  }
+  secondary = {
+    region = var.target_region
+  }
+  final_snapshot_identifier = "${var.global_cluster_identifier}-${formatdate("YYYYMMDDhhmm", timestamp())}"
+}
