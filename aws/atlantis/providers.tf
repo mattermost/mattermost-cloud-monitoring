9f28b1a4cbfa338b1be074be529ff5f092155334
Daniel Wierdsma <dan.wierdsma@gmail.com>
2022-06-15 08:56:38 -0400

    Remove Atlantis helm chart and provider

--- aws/atlantis/providers.tf
+++ aws/atlantis/providers.tf
@@ -10,14 +10,4 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster_auth.token
-  config_path            = "${var.kubeconfig_dir}/kubeconfig"
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster_auth.token
-    config_path            = "${var.kubeconfig_dir}/kubeconfig"
-  }
 }
