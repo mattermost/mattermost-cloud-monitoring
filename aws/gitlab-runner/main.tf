b7f2ef86bb7a824ba009f259a30480bcb327ac12
Daniel Wierdsma <dan.wierdsma@gmail.com>
2022-06-23 14:42:55 -0400

    Separate s3 bucket attributes

--- aws/gitlab-runner/main.tf
+++ aws/gitlab-runner/main.tf
@@ -1,22 +1,27 @@
 resource "aws_s3_bucket" "gitlab_runners" {
   bucket = "mattermost-cloud-gitlab-runners"
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = data.aws_kms_key.master_s3.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-
-  versioning {
-    enabled    = true
-    mfa_delete = false
-  }
 }
 
 resource "aws_s3_bucket_acl" "gitlab_runners" {
   bucket = aws_s3_bucket.gitlab_runners.id
   acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "gitlab_runners" {
+  bucket = aws_s3_bucket.gitlab_runners.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = data.aws_kms_key.master_s3.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "gitlab_runners" {
+  bucket = aws_s3_bucket.gitlab_runners.id
+  versioning_configuration {
+    enabled    = "Enabled"
+    mfa_delete = "Disabled"
+  }
 }
