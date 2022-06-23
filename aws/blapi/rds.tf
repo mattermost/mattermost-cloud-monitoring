5069174196ba6cecd790b6ed02a6d692ad17d489
Daniel Wierdsma <dan.wierdsma@gmail.com>
2022-06-23 15:03:34 -0400

    Revert db_name to name

--- aws/blapi/rds.tf
+++ aws/blapi/rds.tf
@@ -55,7 +55,7 @@ resource "aws_db_instance" "blapi" {
   engine                      = "postgres"
   engine_version              = var.db_engine_version
   instance_class              = var.db_instance_class
-  db_name                     = var.db_name
+  name                        = var.db_name
   username                    = var.db_username
   password                    = var.db_password
   allow_major_version_upgrade = false
--- aws/chaos-engine/main.tf
+++ aws/chaos-engine/main.tf
@@ -70,7 +70,7 @@ resource "aws_db_instance" "chaos_engine" {
   engine                      = "postgres"
   engine_version              = var.db_engine_version
   instance_class              = var.db_instance_class
-  db_name                     = var.db_name
+  name                        = var.db_name
   username                    = var.db_username
   password                    = var.db_password
   allow_major_version_upgrade = false
