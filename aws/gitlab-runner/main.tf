709316825e7f9caf3bc0cd3b57f0df8c8fb6580a
Daniel Wierdsma <dan.wierdsma@gmail.com>
2022-06-23 14:45:42 -0400

    Revert separate s3 bucket attributes

--- aws/gitlab-runner/main.tf
+++ aws/gitlab-runner/main.tf
@@ -1,27 +1,18 @@
 resource "aws_s3_bucket" "gitlab_runners" {
   bucket = "mattermost-cloud-gitlab-runners"
-}
-
-resource "aws_s3_bucket_acl" "gitlab_runners" {
-  bucket = aws_s3_bucket.gitlab_runners.id
   acl    = "private"
-}
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "gitlab_runners" {
-  bucket = aws_s3_bucket.gitlab_runners.bucket
-
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = data.aws_kms_key.master_s3.arn
-      sse_algorithm     = "aws:kms"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = data.aws_kms_key.master_s3.arn
+        sse_algorithm     = "aws:kms"
+      }
     }
   }
-}
 
-resource "aws_s3_bucket_versioning" "gitlab_runners" {
-  bucket = aws_s3_bucket.gitlab_runners.id
-  versioning_configuration {
-    status     = "Enabled"
-    mfa_delete = "Disabled"
+  versioning {
+    enabled    = true
+    mfa_delete = false
   }
 }
