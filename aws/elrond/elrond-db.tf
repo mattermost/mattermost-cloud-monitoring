d06ede72bc6a6fd8b616b41f3b7b8933ba9a1f37
Stavros Foteinopoulos <stafot@gmail.com>
2022-11-15 14:40:13 +0200

    more fixes
    Signed-off-by: Stavros Foteinopoulos <stafot@gmail.com>

--- aws/elrond/elrond-db.tf
+++ aws/elrond/elrond-db.tf
@@ -91,7 +91,6 @@ resource "aws_db_instance" "elrond_read_replica" {
   count = var.enable_elrond_read_replica ? 1 : 0
 
   identifier                  = local.db_identifier_read_replica
-  db_name                      = var.db_name
   instance_class              = var.db_instance_class
   storage_type                = "gp2"
   storage_encrypted           = var.storage_encrypted
