f1ab3ddc8efef1e248011abb79ea766b6b2487c3
Stavros Foteinopoulos <stafot@gmail.com>
2022-11-18 09:32:27 +0200

    Cleanup legacy databases for internal services (#480)
    Blapi, Elrond
    
    Issue: CLD-4009
    Signed-off-by: Stavros Foteinopoulos <stafot@gmail.com>
    
    Signed-off-by: Stavros Foteinopoulos <stafot@gmail.com>

--- aws/awat/locals.tf
+++ aws/awat/locals.tf
@@ -13,7 +13,3 @@ locals {
   conditional_dash_region = data.aws_region.current.name == "us-east-1" ? "" : "-${data.aws_region.current.name}"
   timestamp_now           = formatdate("YYYY-MM-DD-hh-mm", timestamp())
 }
-
-locals {
-  db_identifier_read_replica = "${var.awat_db_identifier}-read"
-}
--- aws/elrond/variables.tf
+++ aws/elrond/variables.tf
@@ -13,41 +13,6 @@ variable "cloud_vpn_cidr" {
   description = "The cidr of the Cloud VPN to allow access from"
 }
 
-variable "db_identifier" {
-  type        = string
-  description = "The Elrond DB identifier"
-}
-
-variable "allocated_db_storage" {
-  type        = string
-  description = "The Elrond DB allocated storage"
-}
-
-variable "db_engine_version" {
-  type        = string
-  description = "The Elrond DB engine"
-}
-
-variable "db_instance_class" {
-  type        = string
-  description = "The Elrond DB instance class"
-}
-
-variable "db_master_az" {
-  type        = string
-  description = "The Elrond DB master node AZ"
-}
-
-variable "db_read_replica_az" {
-  type        = string
-  description = "The Elrond DB read replica AZ"
-}
-
-variable "db_name" {
-  type        = string
-  description = "The Elrond DB name"
-}
-
 variable "db_username" {
   type        = string
   description = "The Elrond DB username"
@@ -73,24 +38,9 @@ variable "db_maintenance_window" {
   description = "The Elrond DB maintenance window"
 }
 
-variable "snapshot_identifier" {
-  type        = string
-  description = "The Elrond DB snapshot identifier"
-}
-
-variable "storage_encrypted" {
-  type        = bool
-  description = "Whether to encrypt DB data or not"
-}
-
 variable "private_subnets" {
   type        = list(string)
   description = "The Elrond DB private subnets"
-}
-
-variable "enable_elrond_read_replica" {
-  type        = bool
-  description = "Whether to encrypt DB data or not"
 }
 
 variable "db_deletion_protection" {
