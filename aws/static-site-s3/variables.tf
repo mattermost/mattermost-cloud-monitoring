b9925a0ab9821e5dd153a7c8e52910350841f9ad
Stavros Foteinopoulos <stafot@gmail.com>
2022-11-23 16:25:50 +0200

    Module S3 static site (#483)
    * Module S3 static site
    
    Issue:
    
    * Remove deprecated definition
    
    Issue:

---
+++ aws/static-site-s3/variables.tf
@@ -0,0 +1,18 @@
+variable "domain_name" {
+  type = string
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resource"
+  type        = map(string)
+  default     = {}
+}
+
+variable "force_destroy" {
+  type    = bool
+  default = false
+}
+
+variable "folder_name" {
+  type = string
+}
