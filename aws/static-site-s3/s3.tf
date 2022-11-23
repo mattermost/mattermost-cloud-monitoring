9eb5fb6370e7ac3f48cba23b1153c53d6f79880a
Stavros Foteinopoulos <stafot@gmail.com>
2022-11-23 15:57:08 +0200

    Remove deprecated definition
    Issue:

--- aws/static-site-s3/s3.tf
+++ aws/static-site-s3/s3.tf
@@ -2,10 +2,6 @@ resource "aws_s3_bucket" "website_bucket" {
   bucket = var.domain_name
   acl    = "public-read"
   policy = data.aws_iam_policy_document.website_policy.json
-  website {
-
-    error_document = "index.html"
-  }
 
   tags = var.tags
 
