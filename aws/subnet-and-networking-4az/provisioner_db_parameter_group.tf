1aefbc8ceb2b22291567adc4efc49e331cb49303
Stylianos Rigas <stylianosrigas@gmail.com>
2022-12-06 12:38:43 +0200

    [CLD-4646] Add missing parameters in MySQL parameter group (#496)
    * CLD-4646 Add missing paramters in MySQL parameter group
    
    * CLD-4646 Apply change in all modules
    
    Co-authored-by: Stylianos Rigas <stylianosrigas@Stylianoss-MacBook-Pro-Work.local>

--- aws/subnet-and-networking-4az/provisioner_db_parameter_group.tf
+++ aws/subnet-and-networking-4az/provisioner_db_parameter_group.tf
@@ -4,6 +4,12 @@ resource "aws_db_parameter_group" "db_parameter_group" {
   name   = "mattermost-provisioner-rds-pg"
   family = "aurora-mysql5.7"
 
+  parameter {
+    apply_method = "immediate"
+    name         = "max_connections"
+    value        = 16000
+  }
+
   tags = merge(
     {
       "MattermostCloudInstallationDatabase" = "MYSQL/Aurora"
@@ -23,6 +29,25 @@ resource "aws_rds_cluster_parameter_group" "cluster_parameter_group" {
     name         = "binlog_format"
     value        = "MIXED"
   }
+
+  parameter {
+    apply_method = "immediate"
+    name         = "max_connections"
+    value        = 16000
+  }
+
+  parameter {
+    apply_method = "immediate"
+    name         = "long_query_time"
+    value        = 5
+  }
+
+  parameter {
+    apply_method = "immediate"
+    name         = "slow_query_log"
+    value        = 1
+  }
+
   tags = merge(
     {
       "MattermostCloudInstallationDatabase" = "MYSQL/Aurora"
