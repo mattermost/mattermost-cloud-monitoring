57f93843818aef85aa21a30a8bf5fb7680782e33
andrleite <andrleite@gmail.com>
2023-01-17 18:53:32 -0300

    [CLD-4578]: Remove users read-only tf module

--- aws/provisioner-read-only-users/providers.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 1.1.8"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws",
-      version = ">= 3.55"
-    }
-  }
-}
--- aws/provisioner-read-only-users/provisioner_users.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_iam_user" "provisioner_users" {
-  for_each = toset(var.provisioner_users)
-  name     = each.value
-  path     = "/"
-
-}
