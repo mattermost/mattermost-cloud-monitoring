636b9f992609949417af73839add39c876593f83
Andre Leite <andrleite@gmail.com>
2023-01-19 18:55:14 -0300

    [CLD-4578]: Remove users read-only tf module (#505)

--- aws/provisioner-read-only-users/example/main.tf
+++
@@ -1,6 +0,0 @@
-module "provisioner-read-only-users" {
-  source                     = "git::https://github.com/mattermost/mattermost-cloud-monitoring.git//aws/provisioner-read-only-users?ref=v1.0.0"
-  environment                = var.environment
-  provisioner_users          = var.provisioner_read_only_users
-  deployment_name            = var.deployment_name
-}
--- aws/provisioner-read-only-users/locals.tf
+++
@@ -1,6 +0,0 @@
-data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
-
-locals {
-  conditional_dash_region = data.aws_region.current.name == "us-east-1" ? "" : "-${data.aws_region.current.name}"
-}
