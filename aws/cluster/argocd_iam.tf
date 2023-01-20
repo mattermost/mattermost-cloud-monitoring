c58297d23c27b34646069f6503620ff0ff6c7ecb
Daniel Wierdsma <dan.wierdsma@gmail.com>
2023-01-20 09:15:48 -0500

    Add new argocd deployer role for ArgoCD cluster access (#507)

---
+++ aws/cluster/argocd_iam.tf
@@ -0,0 +1,19 @@
+#############» ArgoCD Deployer IAM Role###################
+resource "aws_iam_role" "argocd-deployer" {
+  name = "ArgoCD-Deployer"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
