e00b52bb22c9ee96007d6277d0c1405448157960
Daniel Wierdsma <dan.wierdsma@gmail.com>
2023-02-08 10:35:41 -0500

    Add tempo S3 bucket and IAM user

---
+++ aws/tempo/tempo_iam.tf
@@ -0,0 +1,55 @@
+resource "aws_iam_user" "tempo_user" {
+  name = "mattermost-tempo-${var.environment}"
+  path = "/"
+}
+
+resource "aws_iam_policy" "s3" {
+  name        = "mattermost-tempo-route53-policy${local.conditional_dash_region}"
+  path        = "/"
+  description = "Route53 permissions for tempo user"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+         {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListAllMyBuckets"
+            ],
+            "Resource": "arn:aws:s3:::*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket",
+                "s3:GetBucketLocation",
+                "s3:GetBucketTagging"
+            ],
+            "Resource": [
+                "arn:aws:s3:::cloud-tempo-${var.environment}"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject",
+                "s3:PutObjectAcl",
+                "s3:GetObject",
+                "s3:GetObjectAcl",
+                "s3:DeleteObject",
+                "s3:GetObjectVersionAcl"
+            ],
+            "Resource": [
+                "arn:aws:s3:::cloud-tempo-${var.environment}/*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_user_policy_attachment" "attach_s3" {
+  user       = aws_iam_user.tempo_user.name
+  policy_arn = aws_iam_policy.s3.arn
+}
---
+++ aws/tempo/variables.tf
@@ -0,0 +1,11 @@
+variable "environment" {
+  type = string
+}
+
+variable "enable_tempo_bucket_restriction" {
+  type = bool
+}
+
+variable "cnc_user" {
+  type = string
+}
