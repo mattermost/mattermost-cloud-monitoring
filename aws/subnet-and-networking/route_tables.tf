cc9701bceae78b70ae2f4e7ef884e412cd7cafab
Stylianos Rigas <stylianosrigas@Stylianoss-MacBook-Pro-Work.local>
2023-03-15 14:58:22 +0200

    CLD-5258 Second phase of nat gateways with route table switch

--- aws/subnet-and-networking/route_tables.tf
+++ aws/subnet-and-networking/route_tables.tf
@@ -644,85 +644,85 @@ resource "aws_route" "transit_gateway_public_gitlab_1f" {
 }
 
 resource "aws_route_table_association" "private_1a" {
-  for_each = var.single_route_table_deployment == true ? toset(var.vpc_cidrs) : []
+  for_each = toset(var.vpc_cidrs)
 
   subnet_id      = aws_subnet.private_1a[each.value]["id"]
-  route_table_id = aws_route_table.private[each.value]["id"]
+  route_table_id = var.multi_route_table_deployment == true ? aws_route_table.private_1a[each.value]["id"] : aws_route_table.private[each.value]["id"]
 }
 
 resource "aws_route_table_association" "private_1b" {
-  for_each = var.single_route_table_deployment == true ? toset(var.vpc_cidrs) : []
+  for_each = toset(var.vpc_cidrs)
 
   subnet_id      = aws_subnet.private_1b[each.value]["id"]
-  route_table_id = aws_route_table.private[each.value]["id"]
+  route_table_id = var.multi_route_table_deployment == true ? aws_route_table.private_1b[each.value]["id"] : aws_route_table.private[each.value]["id"]
 }
 
 resource "aws_route_table_association" "private_1c" {
-  for_each = var.single_route_table_deployment == true ? toset(var.vpc_cidrs) : []
+  for_each = toset(var.vpc_cidrs)
 
   subnet_id      = aws_subnet.private_1c[each.value]["id"]
-  route_table_id = aws_route_table.private[each.value]["id"]
+  route_table_id = var.multi_route_table_deployment == true ? aws_route_table.private_1c[each.value]["id"] : aws_route_table.private[each.value]["id"]
 }
 
 resource "aws_route_table_association" "private_1d" {
-  for_each = var.single_route_table_deployment == true ? toset(var.vpc_cidrs) : []
+  for_each = toset(var.vpc_cidrs)
 
   subnet_id      = aws_subnet.private_1d[each.value]["id"]
-  route_table_id = aws_route_table.private[each.value]["id"]
+  route_table_id = var.multi_route_table_deployment == true ? aws_route_table.private_1d[each.value]["id"] : aws_route_table.private[each.value]["id"]
 }
 
 resource "aws_route_table_association" "private_1e" {
-  for_each = var.single_route_table_deployment == true ? toset(var.vpc_cidrs) : []
+  for_each = toset(var.vpc_cidrs)
 
   subnet_id      = aws_subnet.private_1e[each.value]["id"]
-  route_table_id = aws_route_table.private[each.value]["id"]
+  route_table_id = var.multi_route_table_deployment == true ? aws_route_table.private_1e[each.value]["id"] : aws_route_table.private[each.value]["id"]
 }
 
 resource "aws_route_table_association" "private_1f" {
-  for_each = var.single_route_table_deployment == true ? toset(var.vpc_cidrs) : []
+  for_each = toset(var.vpc_cidrs)
 
   subnet_id      = aws_subnet.private_1f[each.value]["id"]
-  route_table_id = aws_route_table.private[each.value]["id"]
+  route_table_id = var.multi_route_table_deployment == true ? aws_route_table.private_1f[each.value]["id"] : aws_route_table.private[each.value]["id"]
 }
 
 resource "aws_route_table_association" "public_1a" {
-  for_each = var.single_route_table_deployment == true ? toset(var.vpc_cidrs) : []
+  for_each = toset(var.vpc_cidrs)
 
   subnet_id      = aws_subnet.public_1a[each.value]["id"]
-  route_table_id = aws_route_table.public[each.value]["id"]
+  route_table_id = var.multi_route_table_deployment == true ? aws_route_table.public_1a[each.value]["id"] : aws_route_table.public[each.value]["id"]
 }
 
 resource "aws_route_table_association" "public_1b" {
-  for_each = var.single_route_table_deployment == true ? toset(var.vpc_cidrs) : []
+  for_each = toset(var.vpc_cidrs)
 
   subnet_id      = aws_subnet.public_1b[each.value]["id"]
-  route_table_id = aws_route_table.public[each.value]["id"]
+  route_table_id = var.multi_route_table_deployment == true ? aws_route_table.public_1b[each.value]["id"] : aws_route_table.public[each.value]["id"]
 }
 
 resource "aws_route_table_association" "public_1c" {
-  for_each = var.single_route_table_deployment == true ? toset(var.vpc_cidrs) : []
+  for_each = toset(var.vpc_cidrs)
 
   subnet_id      = aws_subnet.public_1c[each.value]["id"]
-  route_table_id = aws_route_table.public[each.value]["id"]
+  route_table_id = var.multi_route_table_deployment == true ? aws_route_table.public_1c[each.value]["id"] : aws_route_table.public[each.value]["id"]
 }
 
 resource "aws_route_table_association" "public_1d" {
-  for_each = var.single_route_table_deployment == true ? toset(var.vpc_cidrs) : []
+  for_each = toset(var.vpc_cidrs)
 
   subnet_id      = aws_subnet.public_1d[each.value]["id"]
-  route_table_id = aws_route_table.public[each.value]["id"]
+  route_table_id = var.multi_route_table_deployment == true ? aws_route_table.public_1d[each.value]["id"] : aws_route_table.public[each.value]["id"]
 }
 
 resource "aws_route_table_association" "public_1e" {
-  for_each = var.single_route_table_deployment == true ? toset(var.vpc_cidrs) : []
+  for_each = toset(var.vpc_cidrs)
 
   subnet_id      = aws_subnet.public_1e[each.value]["id"]
-  route_table_id = aws_route_table.public[each.value]["id"]
+  route_table_id = var.multi_route_table_deployment == true ? aws_route_table.public_1e[each.value]["id"] : aws_route_table.public[each.value]["id"]
 }
 
 resource "aws_route_table_association" "public_1f" {
-  for_each = var.single_route_table_deployment == true ? toset(var.vpc_cidrs) : []
+  for_each = toset(var.vpc_cidrs)
 
   subnet_id      = aws_subnet.public_1f[each.value]["id"]
-  route_table_id = aws_route_table.public[each.value]["id"]
+  route_table_id = var.multi_route_table_deployment == true ? aws_route_table.public_1f[each.value]["id"] : aws_route_table.public[each.value]["id"]
 }
