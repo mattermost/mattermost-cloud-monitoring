3c1b95cc79b9cf7d07c1b961759e02787cfc63cf
Daniel Wierdsma <dan.wierdsma@gmail.com>
2023-04-03 12:36:50 -0400

    Add cloud lambda bucket storage (#552)
    * Add cloud lambda bucket storage
    
    * Add environments
    
    * Add new versioning format
    
    * Add linting
    
    * Add tf docs

---
+++ aws/cloud-lambda-storage/s3.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "lambdas_bucket" {
+  bucket = "mattermost-cloud-lambdas-${var.environment}"
+}
+
+resource "aws_s3_bucket_acl" "lambdas_bucket" {
+  bucket = aws_s3_bucket.lambdas_bucket.id
+
+  acl = "private"
+}
+
+resource "aws_s3_bucket_versioning" "lambdas_bucket" {
+  bucket = aws_s3_bucket.lambdas_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
---
+++ aws/cloud-lambda-storage/variables.tf
@@ -0,0 +1,4 @@
+variable "environment" {
+  type        = string
+  description = "The name of the environment which will deploy to and will be added as a tag"
+}
