d237dc55e4ab4840f00db84d4ceae51e41449624
Daniel Wierdsma <dan.wierdsma@gmail.com>
2023-04-04 11:46:06 -0400

    Fix some aws provider straglers (#554)

--- aws/aurora-cluster/providers.tf
+++ aws/aurora-cluster/providers.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 1.1.8"
   required_providers {
     aws = {
-      source  = "hashicorp/aws",
-      version = ">= 4.12"
+      source  = "hashicorp/aws"
+      version = ">= 4.61.0"
     }
     random = {
       source  = "hashicorp/random"
--- aws/ebs-janitor/providers.tf
+++ aws/ebs-janitor/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.8"
   required_providers {
     aws = {
-      source  = "hashicorp/aws",
+      source  = "hashicorp/aws"
       version = ">= 4.61.0"
     }
   }
