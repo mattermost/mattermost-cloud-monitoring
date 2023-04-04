05927ce2fb48d4da38c37d38029ed532468fe955
Daniel Wierdsma <dan.wierdsma@gmail.com>
2023-04-04 11:53:23 -0400

    Upgrade tf minimum versions

--- aws/transit-gateway-attachment/versions.tf
+++ aws/transit-gateway-attachment/versions.tf
@@ -1,9 +1,9 @@
 terraform {
+  required_version = ">= 1.1.8"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.55"
+      version = ">= 4.61.0"
     }
   }
-  required_version = ">= 0.13"
 }
