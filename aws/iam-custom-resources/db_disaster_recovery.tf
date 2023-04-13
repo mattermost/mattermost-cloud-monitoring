082f7bef988e80a32aa4423f539d2ba72dfe32b9
Muhammad Shahid <muhammad.shahid@mattermost.com>
2023-04-13 14:33:20 +1000

    Removed extra lines

--- aws/iam-custom-resources/db_disaster_recovery.tf
+++ aws/iam-custom-resources/db_disaster_recovery.tf
@@ -7,7 +7,7 @@ resource "aws_iam_user" "db_disaster_recovery" {
 
 resource "aws_iam_policy" "db_disaster_recovery" {
   count = var.create_db_disaster_user ? 1 : 0
-  
+
   name        = "mattermost-cloud-${var.environment}-db-disaster-recovery-policy"
   description = "A policy attached to DB Disaster Recovery IAM user"
   path        = "/"
@@ -32,4 +32,3 @@ resource "aws_iam_user_policy_attachment" "db_disaster_recovery" {
   user       = aws_iam_user.db_disaster_recovery[0].name
   policy_arn = aws_iam_policy.db_disaster_recovery[0].arn
 }
-
