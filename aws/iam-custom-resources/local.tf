be218ec8be13aeddee02734cfb4c9388bdb60ea3
Muhammad Shahid <muhammad.shahid@mattermost.com>
2023-04-17 12:11:59 +1000

    Fixed secret manager arn format

--- aws/iam-custom-resources/local.tf
+++ aws/iam-custom-resources/local.tf
@@ -4,6 +4,6 @@ data "aws_region" "current" {}
 
 locals {
 
-  secret_manager_rds = "arn:aws:secretsmanage:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret/rds-cluster-multitenant-*"
+  secret_manager_rds = "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:rds-cluster-multitenant-*"
 
 }
