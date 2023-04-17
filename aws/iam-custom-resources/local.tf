648903f1e049dd1a1731aa9b5cf6ca0da89c0016
Muhammad Shahid <muhammad.shahid@mattermost.com>
2023-04-17 11:47:36 +1000

    Typo fix for Added secret manager read only permission

--- aws/iam-custom-resources/local.tf
+++ aws/iam-custom-resources/local.tf
@@ -4,7 +4,7 @@ data "aws_region" "current" {}
 
 locals {
 
-  secret_manager_rds = "arn:aws:secretsmanage:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret/rds-cluster-multitenant-*"
+  secret_manager_rds = "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret/rds-cluster-multitenant-*"
 }
 
 output "name" {
