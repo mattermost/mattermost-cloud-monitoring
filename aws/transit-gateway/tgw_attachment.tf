0ea445d7bf7d0c8a2b08cbcb43c88e1d6bf8ea98
Stylianos Rigas <stylianosrigas@Stylianoss-MacBook-Pro-Work.local>
2023-04-19 15:24:16 +0300

    CLD-5482 Add support for multiple route tables in transit gateway attachment module

--- aws/transit-gateway/tgw_attachment.tf
+++ aws/transit-gateway/tgw_attachment.tf
@@ -5,42 +5,48 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment" {
 }
 
 resource "aws_route" "private_tgw_attachment_route" {
-  route_table_id         = var.private_route_table_id
+  for_each               = toset(var.private_route_table_ids)
+  route_table_id         = each.value
   destination_cidr_block = var.transit_gtw_route_destination
   transit_gateway_id     = var.transit_gateway_id
   depends_on             = [aws_ec2_transit_gateway_vpc_attachment.tgw_attachment]
 }
 
 resource "aws_route" "private_tgw_attachment_route_security" {
-  route_table_id         = var.private_route_table_id
+  for_each               = toset(var.private_route_table_ids)
+  route_table_id         = each.value
   destination_cidr_block = var.transit_gtw_route_destination_security
   transit_gateway_id     = var.transit_gateway_id
   depends_on             = [aws_ec2_transit_gateway_vpc_attachment.tgw_attachment]
 }
 
 resource "aws_route" "private_tgw_attachment_route_gitlab" {
-  route_table_id         = var.private_route_table_id
+  for_each               = toset(var.private_route_table_ids)
+  route_table_id         = each.value
   destination_cidr_block = var.transit_gtw_route_destination_gitlab
   transit_gateway_id     = var.transit_gateway_id
   depends_on             = [aws_ec2_transit_gateway_vpc_attachment.tgw_attachment]
 }
 
 resource "aws_route" "public_tgw_attachment_route" {
-  route_table_id         = var.public_route_table_id
+  for_each               = toset(var.public_route_table_ids)
+  route_table_id         = each.value
   destination_cidr_block = var.transit_gtw_route_destination
   transit_gateway_id     = var.transit_gateway_id
   depends_on             = [aws_ec2_transit_gateway_vpc_attachment.tgw_attachment]
 }
 
 resource "aws_route" "public_tgw_attachment_security" {
-  route_table_id         = var.public_route_table_id
+  for_each               = toset(var.public_route_table_ids)
+  route_table_id         = each.value
   destination_cidr_block = var.transit_gtw_route_destination_security
   transit_gateway_id     = var.transit_gateway_id
   depends_on             = [aws_ec2_transit_gateway_vpc_attachment.tgw_attachment]
 }
 
 resource "aws_route" "public_tgw_attachment_gitlab" {
-  route_table_id         = var.public_route_table_id
+  for_each               = toset(var.public_route_table_ids)
+  route_table_id         = each.value
   destination_cidr_block = var.transit_gtw_route_destination_gitlab
   transit_gateway_id     = var.transit_gateway_id
   depends_on             = [aws_ec2_transit_gateway_vpc_attachment.tgw_attachment]
