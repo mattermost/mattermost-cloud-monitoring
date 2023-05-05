a70f149e20ecaa262fe60b99508aade86b48702a
Daniel Wierdsma <dan.wierdsma@gmail.com>
2023-05-05 08:40:01 -0400

    Add apps deployer role to iam-custom-resources

--- aws/iam-custom-resources/variables.tf
+++ aws/iam-custom-resources/variables.tf
@@ -14,3 +14,14 @@ variable "create_db_disaster_user" {
   type        = bool
   default     = false
 }
+
+variable "create_apps_deployer_user" {
+  description = "Whether to create the Apps Deployer user"
+  type        = bool
+  default     = false
+}
+
+variable "apps_deployer_assume_role_arn" {
+  description = "The ARN of the role to assume for the apps deployer user"
+  type        = string
+}
