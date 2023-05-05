8945e5c5082dfbc66174abf238f475c70ed6a8ae
Stylianos Rigas <stylianosrigas@gmail.com>
2023-05-05 14:04:50 +0300

    [CLD-5259] Add vpc S3 endpoint module and update existing subnet modules (#592)
    * CLD-5259 Add vpc S3 endpoint module and update existing subnet modules
    
    * CLD-5259 Tf linit
    
    * CLD-5259 Add provider
    
    ---------
    
    Co-authored-by: Stylianos Rigas <stylianosrigas@Stylianoss-MacBook-Pro-Work.local>

---
+++ aws/subnet-and-networking-4az/vpc_endpoint.tf
@@ -0,0 +1,65 @@
+resource "aws_vpc_endpoint" "s3_us_east_1" {
+  for_each     = toset(var.vpc_cidrs)
+  vpc_id       = each.value
+  service_name = "com.amazonaws.us-east-1.s3"
+
+  tags = {
+    VpcID = each.value
+  }
+}
+
+resource "aws_vpc_endpoint_route_table_association" "endpoint_private_1a" {
+  for_each = toset(var.vpc_cidrs)
+
+  vpc_endpoint_id = aws_vpc_endpoint.s3_us_east_1[each.value]["id"]
+  route_table_id  = var.multi_route_table_deployment == true ? aws_route_table.private_1a[each.value]["id"] : aws_route_table.private[each.value]["id"]
+}
+
+resource "aws_vpc_endpoint_route_table_association" "endpoint_private_1b" {
+  for_each = toset(var.vpc_cidrs)
+
+  vpc_endpoint_id = aws_vpc_endpoint.s3_us_east_1[each.value]["id"]
+  route_table_id  = var.multi_route_table_deployment == true ? aws_route_table.private_1b[each.value]["id"] : aws_route_table.private[each.value]["id"]
+}
+
+resource "aws_vpc_endpoint_route_table_association" "endpoint_private_1c" {
+  for_each = toset(var.vpc_cidrs)
+
+  vpc_endpoint_id = aws_vpc_endpoint.s3_us_east_1[each.value]["id"]
+  route_table_id  = var.multi_route_table_deployment == true ? aws_route_table.private_1c[each.value]["id"] : aws_route_table.private[each.value]["id"]
+}
+
+resource "aws_vpc_endpoint_route_table_association" "endpoint_private_1d" {
+  for_each = toset(var.vpc_cidrs)
+
+  vpc_endpoint_id = aws_vpc_endpoint.s3_us_east_1[each.value]["id"]
+  route_table_id  = var.multi_route_table_deployment == true ? aws_route_table.private_1d[each.value]["id"] : aws_route_table.private[each.value]["id"]
+}
+
+resource "aws_vpc_endpoint_route_table_association" "endpoint_public_1a" {
+  for_each = toset(var.vpc_cidrs)
+
+  vpc_endpoint_id = aws_vpc_endpoint.s3_us_east_1[each.value]["id"]
+  route_table_id  = var.multi_route_table_deployment == true ? aws_route_table.public_1a[each.value]["id"] : aws_route_table.public[each.value]["id"]
+}
+
+resource "aws_vpc_endpoint_route_table_association" "endpoint_public_1b" {
+  for_each = toset(var.vpc_cidrs)
+
+  vpc_endpoint_id = aws_vpc_endpoint.s3_us_east_1[each.value]["id"]
+  route_table_id  = var.multi_route_table_deployment == true ? aws_route_table.public_1b[each.value]["id"] : aws_route_table.public[each.value]["id"]
+}
+
+resource "aws_vpc_endpoint_route_table_association" "endpoint_public_1c" {
+  for_each = toset(var.vpc_cidrs)
+
+  vpc_endpoint_id = aws_vpc_endpoint.s3_us_east_1[each.value]["id"]
+  route_table_id  = var.multi_route_table_deployment == true ? aws_route_table.public_1c[each.value]["id"] : aws_route_table.public[each.value]["id"]
+}
+
+resource "aws_vpc_endpoint_route_table_association" "endpoint_public_1d" {
+  for_each = toset(var.vpc_cidrs)
+
+  vpc_endpoint_id = aws_vpc_endpoint.s3_us_east_1[each.value]["id"]
+  route_table_id  = var.multi_route_table_deployment == true ? aws_route_table.public_1d[each.value]["id"] : aws_route_table.public[each.value]["id"]
+}
---
+++ aws/vpc-endpoint/README.md
@@ -0,0 +1,35 @@
+<!-- BEGIN_TF_DOCS -->
+## Requirements
+
+No requirements.
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_aws"></a> [aws](#provider\_aws) | n/a |
+
+## Modules
+
+No modules.
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [aws_vpc_endpoint.s3_us_east_1](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_endpoint) | resource |
+| [aws_vpc_endpoint_route_table_association.private_s3_endpoint](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_endpoint_route_table_association) | resource |
+| [aws_vpc_endpoint_route_table_association.public_s3_endpoint](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_endpoint_route_table_association) | resource |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_private_route_table_ids"></a> [private\_route\_table\_ids](#input\_private\_route\_table\_ids) | n/a | `list(string)` | n/a | yes |
+| <a name="input_public_route_table_ids"></a> [public\_route\_table\_ids](#input\_public\_route\_table\_ids) | n/a | `list(string)` | n/a | yes |
+| <a name="input_vpc_id"></a> [vpc\_id](#input\_vpc\_id) | n/a | `string` | n/a | yes |
+
+## Outputs
+
+No outputs.
+<!-- END_TF_DOCS -->
