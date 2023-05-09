c453b727bffcd9797615245e8947dd2098e56efe
Muhammad Shahid <53712140+Mshahidtaj@users.noreply.github.com>
2023-05-09 20:55:05 +1000

    Added SG rule to allow cross region access for community/customer DB (#597)
    * Added SG rule to allow cross region access for customer DB
    
    * Modified the variable description in order to be consistent with other variables
    
    * updated the readme to reflect the changes

--- aws/subnet-and-networking/security_groups.tf
+++ aws/subnet-and-networking/security_groups.tf
@@ -223,6 +223,18 @@ resource "aws_security_group_rule" "db_ingress_worker_command_control" {
   type              = "ingress"
 }
 
+resource "aws_security_group_rule" "db_ingress_from_crossregion" {
+  for_each = toset(var.vpc_cidrs)
+
+  cidr_blocks       = var.cross_region_vpc_cidrs_db_access
+  description       = "Ingress Traffic from Cross region VPCs"
+  from_port         = 3306
+  protocol          = "TCP"
+  security_group_id = aws_security_group.db_sg[each.value]["id"]
+  to_port           = 3306
+  type              = "ingress"
+}
+
 resource "aws_security_group_rule" "developers_vpn_access" {
   for_each = toset(var.vpc_cidrs)
 
