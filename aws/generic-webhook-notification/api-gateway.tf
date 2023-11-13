a97662e269a6b51e6fa55d925e3ac862eb4af6b2
Stavros Foteinopoulos <stafot@gmail.com>
2023-11-13 18:00:47 +0200

    Create module for generic-webhook-notification
    Signed-off-by: Stavros Foteinopoulos <stafot@gmail.com>

---
+++ aws/generic-webhook-notification/api-gateway.tf
@@ -0,0 +1,73 @@
+resource "aws_api_gateway_rest_api" "core-generic-webhook-notification" {
+  name        = "core-generic-webhook-notification"
+  description = "Managed by Terraform"
+}
+
+resource "aws_api_gateway_resource" "provisioner-notification-resource" {
+  rest_api_id = aws_api_gateway_rest_api.core-generic-webhook-notification.id
+  parent_id   = aws_api_gateway_rest_api.core-generic-webhook-notification.root_resource_id
+  path_part   = "notification"
+}
+
+resource "aws_api_gateway_method" "notification-post" {
+  rest_api_id   = aws_api_gateway_rest_api.core-generic-webhook-notification.id
+  resource_id   = aws_api_gateway_resource.provisioner-notification-resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "notification-integration" {
+  rest_api_id = aws_api_gateway_rest_api.core-generic-webhook-notification.id
+  resource_id = aws_api_gateway_resource.provisioner-notification-resource.id
+  http_method = aws_api_gateway_method.notification-post.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.provisioner-notification.invoke_arn
+}
+
+resource "aws_api_gateway_resource" "elrond-notification-resource" {
+  rest_api_id = aws_api_gateway_rest_api.core-generic-webhook-notification.id
+  parent_id   = aws_api_gateway_rest_api.core-generic-webhook-notification.root_resource_id
+  path_part   = "elrond-notification"
+}
+
+resource "aws_api_gateway_method" "elrond-notification-post" {
+  rest_api_id   = aws_api_gateway_rest_api.core-generic-webhook-notification.id
+  resource_id   = aws_api_gateway_resource.elrond-notification-resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "elrond-notification-integration" {
+  rest_api_id = aws_api_gateway_rest_api.core-generic-webhook-notification.id
+  resource_id = aws_api_gateway_resource.elrond-notification-resource.id
+  http_method = aws_api_gateway_method.elrond-notification-post.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.elrond-notification.invoke_arn
+}
+
+resource "aws_api_gateway_resource" "gitlab-webhook-resource" {
+  rest_api_id = aws_api_gateway_rest_api.core-generic-webhook-notification.id
+  parent_id   = aws_api_gateway_rest_api.core-generic-webhook-notification.root_resource_id
+  path_part   = "gitlab-webhook"
+}
+
+resource "aws_api_gateway_method" "gitlab-webhook-post" {
+  rest_api_id   = aws_api_gateway_rest_api.core-generic-webhook-notification.id
+  resource_id   = aws_api_gateway_resource.gitlab-webhook-resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "gitlab-webhook-integration" {
+  rest_api_id = aws_api_gateway_rest_api.core-generic-webhook-notification.id
+  resource_id = aws_api_gateway_resource.gitlab-webhook-resource.id
+  http_method = aws_api_gateway_method.gitlab-webhook-post.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.gitlab-webhook.invoke_arn
+}
---
+++ aws/generic-webhook-notification/lambdas.tf
@@ -0,0 +1,89 @@
+resource "aws_lambda_function" "provisioner-notification" {
+  function_name = "provisioner-notification"
+  runtime       = "provided.al2"
+  memory_size   = 128
+  timeout       = 120
+  handler       = "bootstrap"
+
+  s3_bucket = var.lambda_s3_bucket
+  s3_key    = var.lambda_provisioner_notification_s3_key
+
+  environment {
+    variables = {
+      MATTERMOST_WEBHOOK_PROD       = var.mattermost_webhook_prod
+      MATTERMOST_WEBHOOK_ALERT_PROD = var.mattermost_webhook_alert_prod
+      OPSGENIE_APIKEY               = var.opsgenie_apikey
+      OPSGENIE_SCHEDULER_TEAM       = var.opsgenie_scheduler_team
+    }
+  }
+}
+
+resource "aws_lambda_function" "elrond-notification" {
+  function_name = "elrond-notification"
+  runtime       = "provided.al2"
+  memory_size   = 128
+  timeout       = 120
+  handler       = "bootstrap"
+
+  s3_bucket = var.lambda_s3_bucket
+  s3_key    = var.lambda_elrond_notification_s3_key
+
+  environment {
+    variables = {
+      MATTERMOST_ELROND_WEBHOOK_PROD = var.mattermost_elrond_webhook_prod
+      MATTERMOST_WEBHOOK_ALERT_PROD  = var.mattermost_webhook_alert_prod
+      OPSGENIE_APIKEY                = var.opsgenie_apikey
+      OPSGENIE_SCHEDULER_TEAM        = var.opsgenie_scheduler_team
+      ENVIRONMENT                    = "PROD"
+    }
+  }
+}
+
+resource "aws_lambda_function" "gitlab-webhook" {
+  function_name = "gitlab-webhook"
+  runtime       = "provided.al2"
+  memory_size   = 128
+  timeout       = 120
+  handler       = "bootstrap"
+
+  s3_bucket = var.lambda_s3_bucket
+  s3_key    = var.lambda_gitlab_webhook_s3_key
+
+  environment {
+    variables = {
+      MATTERMOST_NOTIFICATION_HOOK = var.mattermost_notification_hook
+    }
+  }
+}
+
+resource "aws_lambda_permission" "provisioner-notification-permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.provisioner-notification.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.core-generic-webhook-notification.execution_arn}/*/${aws_api_gateway_method.notification-post.http_method}/${aws_api_gateway_resource.provisioner-notification-resource.path_part}"
+}
+
+
+resource "aws_lambda_permission" "elrond-notification-permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.elrond-notification.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.core-generic-webhook-notification.execution_arn}/*/${aws_api_gateway_method.elrond-notification-post.http_method}/${aws_api_gateway_resource.elrond-notification-resource.path_part}"
+}
+
+resource "aws_lambda_permission" "gitlab-webhook-permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.gitlab-webhook.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.core-generic-webhook-notification.execution_arn}/*/${aws_api_gateway_method.gitlab-webhook-post.http_method}/${aws_api_gateway_resource.gitlab-webhook-resource.path_part}"
+}
+
+
+
+
