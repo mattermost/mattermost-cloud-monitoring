9d9814dd7c75a5f6a7e760af4cac8aea8bb6a5f1
Stavros Foteinopoulos <stafot@gmail.com>
2023-12-13 11:50:05 +0200

    Fix resolve conflicts on eks addons
    Signed-off-by: Stavros Foteinopoulos <stafot@gmail.com>

--- aws/eks-cluster/eks_addon.tf
+++ aws/eks-cluster/eks_addon.tf
@@ -1,9 +1,11 @@
 resource "aws_eks_addon" "vpc_cni" {
   count = var.enable_vpc_cni_addon ? 1 : 0
 
-  cluster_name  = aws_eks_cluster.cluster.name
-  addon_name    = "vpc-cni"
-  addon_version = var.vpc_cni_addon_version
+  cluster_name                = aws_eks_cluster.cluster.name
+  addon_name                  = "vpc-cni"
+  addon_version               = var.vpc_cni_addon_version
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
 
   depends_on = [
     aws_iam_role_policy_attachment.cluster-AmazonEKSCNIPolicy
@@ -13,27 +15,30 @@ resource "aws_eks_addon" "vpc_cni" {
 resource "aws_eks_addon" "coredns" {
   count = var.enable_coredns_addon ? 1 : 0
 
-  cluster_name      = aws_eks_cluster.cluster.name
-  addon_name        = "coredns"
-  addon_version     = var.coredns_addon_version
-  resolve_conflicts = "OVERWRITE"
+  cluster_name                = aws_eks_cluster.cluster.name
+  addon_name                  = "coredns"
+  addon_version               = var.coredns_addon_version
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
 }
 
 
 resource "aws_eks_addon" "kube_proxy" {
   count = var.enable_kube_proxy_addon ? 1 : 0
 
-  cluster_name      = aws_eks_cluster.cluster.name
-  addon_name        = "kube-proxy"
-  addon_version     = var.kube_proxy_addon_version
-  resolve_conflicts = "OVERWRITE"
+  cluster_name                = aws_eks_cluster.cluster.name
+  addon_name                  = "kube-proxy"
+  addon_version               = var.kube_proxy_addon_version
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
 }
 
 resource "aws_eks_addon" "ebs_csi" {
   count = var.enable_ebs_csi_addon ? 1 : 0
 
-  cluster_name      = aws_eks_cluster.cluster.name
-  addon_name        = "aws-ebs-csi-driver"
-  addon_version     = var.ebs_csi_addon_version
-  resolve_conflicts = "OVERWRITE"
+  cluster_name                = aws_eks_cluster.cluster.name
+  addon_name                  = "aws-ebs-csi-driver"
+  addon_version               = var.ebs_csi_addon_version
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
 }
