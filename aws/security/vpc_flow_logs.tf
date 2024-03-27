4692fec7462993dd03fec928c9e1d63f082c4eaf
Angelos Kyratzakos <angelos.kyratzakos@mattermost.com>
2024-03-27 11:58:23 +0200

    Remove BLAPI residuals from all terraform modules, change security flow logs from list to map (#679)
    * remove blapi
    
    * change security module for flow logs from list to map

--- aws/security/vpc_flow_logs.tf
+++ aws/security/vpc_flow_logs.tf
@@ -1,10 +1,9 @@
 data "aws_vpcs" "vpcs" {}
 
 resource "aws_flow_log" "cloud_vpc" {
-  count                = var.enable_flow_logs ? length(data.aws_vpcs.vpcs.ids) : 0
+  for_each             = var.enable_flow_logs ? { for vpc_id in data.aws_vpcs.vpcs.ids : vpc_id => vpc_id } : {}
   log_destination      = "arn:aws:s3:::mm-flowlogs-${var.vpc_flow_logs_env}"
   log_destination_type = "s3"
   traffic_type         = "ALL"
-  vpc_id               = element(data.aws_vpcs.vpcs.ids[*], count.index)
+  vpc_id               = each.key
 }
-
