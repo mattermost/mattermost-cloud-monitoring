bdf5558d09a15c06bba5af1d9ee213f6b458b163
Andre Leite <andrleite@gmail.com>
2024-04-04 16:05:04 -0300

    temporary change for db upgrade

--- aws/rds-customer-cluster/variables.tf
+++ aws/rds-customer-cluster/variables.tf
@@ -256,7 +256,7 @@ variable "password_encryption" {
   description = "The password encryption method to use for the DB instance. Valid values: md5 or scram-sha-256"
 }
 
-variable "accepted_password_auth_method" {
-  type        = string
-  description = "The authentication method to use for the DB instance. Valid values: md5+scram or scram"
-}
+# variable "accepted_password_auth_method" {
+#   type        = string
+#   description = "The authentication method to use for the DB instance. Valid values: md5+scram or scram"
+# }
