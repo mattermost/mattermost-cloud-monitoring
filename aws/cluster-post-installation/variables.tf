c714fc4c5705662e79b5007cd08d16dbdc1cd274
Stavros Foteinopoulos <stafot@gmail.com>
2024-08-13 15:58:03 +0300

    Add support for tagging in s3 buckets (#714)
    Signed-off-by: Stavros Foteinopoulos <stafot@gmail.com>

--- aws/cluster-post-installation/variables.tf
+++ aws/cluster-post-installation/variables.tf
@@ -2,3 +2,9 @@ variable "environment" {
   type        = string
   description = "The environment will be created"
 }
+
+variable "tags_metrics_bucket" {
+  type        = map(string)
+  description = "Tags for prometheus metrics s3 bucket"
+}
+
--- aws/tempo/variables.tf
+++ aws/tempo/variables.tf
@@ -6,3 +6,7 @@ variable "enable_tempo_bucket_restriction" {
   type = bool
 }
 
+variable "tempo_bucket_tags" {
+  type        = map(string)
+  description = "Tags for tempo s3 bucket"
+}
