c279fe44f56e1b3400edb9ae7c890c3401933dd8
Angelos Kyratzakos <angelos.kyratzakos@mattermost.com>
2024-08-14 19:25:06 +0300

    pexip infinity setup mvp

---
+++ aws/pexip/ec2.tf
@@ -0,0 +1,72 @@
+resource "aws_network_interface" "pexip_management" {
+  subnet_id       = var.public_subnet_id
+  private_ips     = var.management_private_ips
+  security_groups = [aws_security_group.pexip_management_sg.id]
+}
+
+resource "aws_instance" "pexip_management" {
+  ami      = var.initial_configuration ? var.official_pexip_management_ec2_ami : var.custom_management_ec2_ami
+  instance_type = var.management_ec2_type
+  key_name =  var.initial_configuration ? var.ec2_key_pair : ""
+
+
+  network_interface {
+    network_interface_id = aws_network_interface.pexip_management.id
+    device_index         = 0
+  }
+
+  tags = {
+    "Name" = "${var.name}-management"
+  }
+}
+
+resource "aws_eip_association" "pexip_management" {
+  instance_id   = aws_instance.pexip_management.id
+  allocation_id = aws_eip.pexip_management_eip.id
+}
+
+
+resource "aws_network_interface" "pexip_conference" {
+  subnet_id       = var.public_subnet_id
+  private_ips     = var.conference_private_ips
+  security_groups = [aws_security_group.pexip_conference_sg.id]
+}
+
+resource "aws_instance" "pexip_conference" {
+  ami      = var.initial_configuration ? var.official_pexip_conference_ec2_ami : var.custom_conference_ec2_ami
+  instance_type = var.conference_ec2_type
+  key_name = var.ec2_key_pair
+
+  network_interface {
+    network_interface_id = aws_network_interface.pexip_conference.id
+    device_index         = 0
+  }
+
+  tags = {
+    "Name" = "${var.name}-conference"
+  }
+}
+
+resource "aws_eip_association" "pexip_conference" {
+  instance_id   = aws_instance.pexip_conference.id
+  allocation_id = aws_eip.pexip_conference_eip.id
+}
+
+
+
+
+resource "aws_eip" "pexip_management_eip" {
+  tags = merge(
+    {
+      "Name" = "${var.name}-management-eip"
+    }
+  )
+}
+
+resource "aws_eip" "pexip_conference_eip" {
+  tags = merge(
+    {
+      "Name" = "${var.name}-conference-eip"
+    }
+  )
+}
---
+++ aws/pexip/variables.tf
@@ -0,0 +1,96 @@
+variable "name" {
+  type = string
+  default = "pexip"
+}
+
+variable "region" {
+  type        = string
+  description = "The AWS region which will be used."
+}
+
+variable "public_subnet_id" {
+  type        = string
+  description = "A public subnet ID"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "The ID of the vpc for the security group of Pexip"
+}
+
+variable "cloudflare_zone_id" {
+  type        = string
+  description = "The Cloudflare zone ID provided"
+}
+
+variable "conference_cloudflare_record_name" {
+  type        = string
+  description = "The DNS name for the Pexip conference node"
+}
+
+variable "management_cloudflare_record_name" {
+  type        = string
+  description = "The DNS name for the Pexip management node"
+}
+
+variable "cloudflare_api_token" {
+  type    = string
+}
+
+variable "management_private_ips" {
+  type        = list(string)
+  description = "List of the private IPs of the Pexip management node"
+}
+
+variable "conference_private_ips" {
+  type        = list(string)
+  description = "List of the private IPs of the Pexip Conference node"
+}
+
+variable "vpn_ips" {
+  type        = list(string)
+  description = "List of the IPs for the VPN"
+}
+
+variable "official_pexip_management_ec2_ami" {
+  default     = "ami-0dd1e9ce5c9029446"
+  type        = string
+  description = "The official Pexip AMI for management node"
+}
+
+variable "official_pexip_conference_ec2_ami" {
+  default     = "ami-0ddd16b36dc9f4229"
+  type        = string
+  description = "The official Pexip AMI for conference node"
+}
+
+variable "custom_management_ec2_ami" {
+  type        = string
+  description = "Customized with MM configuration Pexip AMI for management node"
+}
+
+variable "custom_conference_ec2_ami" {
+  type        = string
+  description = "Customized with MM configuration Pexip AMI for conference node"
+}
+
+variable "management_ec2_type" {
+  type        = string
+  description = "The EC2 instance type for Pexip management node"
+}
+
+variable "conference_ec2_type" {
+  type        = string
+  description = "The EC2 instance type for Pexip conference node"
+}
+
+variable "ec2_key_pair" {
+  type        = string
+  description = "The key pair that will be used for ssh to EC2 instances of Pexip nodes"
+}
+
+variable "initial_configuration" {
+  description = "A boolean variable to control the initial configuration of Pexip setup, when true official AMI will be deployed and key-pairs will be added to EC2 nodes"
+  type        = bool
+  default     = true
+}
