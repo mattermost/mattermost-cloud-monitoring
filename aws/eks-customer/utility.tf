b00dcbc46af1f9bdc5725a42e007444b9bc94fee
Andre Leite <andrleite@gmail.com>
2024-09-09 21:39:19 -0300

    [CLD-7696]remove old block

--- aws/eks-customer/utility.tf
+++ aws/eks-customer/utility.tf
@@ -1,25 +1,3 @@
-# module "utilities" {
-#   # source = "github.com/mattermost/mattermost-cloud-monitoring.git//aws/argocd-utility?ref=cld-7696"
-#   source = "/Users/andreleite/code/src/github/mattermost/mattermost-cloud-monitoring/aws/argocd-utility"
-
-#   utilities = var.utilities
-#   cluster_name = var.cluster_name
-#   gitops_repo_url = var.gitops_repo_url
-#   environment = var.environment
-#   lb_certificate_arn = var.lb_certificate_arn
-#   lb_private_certificate_arn = var.lb_private_certificate_arn
-#   vpc_id = var.vpc_id
-#   private_domain = var.private_domain
-#   allow_list_cidr_range = var.allow_list_cidr_range
-#   api_server = module.eks.cluster_endpoint
-#   ca_data = module.eks.cluster_certificate_authority_data
-#   argocd_role_arn = var.argocd_role_arn
-#   oidc_provider_arn = module.eks.oidc_provider_arn
-#   internal_lb_endpoint = "test-endpoint"
-
-#   #depends_on = [ module.managed_node_group ]
-# }
-
 locals {
   internal_domain = "internal.${var.environment}.${var.private_domain}"
 }
