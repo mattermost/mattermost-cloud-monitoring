800ad59b5bfa36dfbaa6e677c89d17045b813cb3
Stavros Foteinopoulos <stafot@gmail.com>
2024-09-11 12:11:27 +0300

    don't assume role upon resources creation
    Signed-off-by: Stavros Foteinopoulos <stafot@gmail.com>

--- aws/cross-account-private-link-eks-private-source-account/providers.tf
+++ aws/cross-account-private-link-eks-private-source-account/providers.tf
@@ -15,9 +15,6 @@ terraform {
 provider "aws" {
   alias  = "source"
   region = data.aws_region.current.name
-  assume_role {
-    role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/SourceAccountTerraformRole"
-  }
 }
 
 
