6b08083b7d105d4338115c239428d96f1f0a8aec
Angelos Kyratzakos <angelos.kyratzakos@mattermost.com>
2024-09-11 19:09:36 +0300

    remove UI ports for management

--- aws/pexip/sg.tf
+++ aws/pexip/sg.tf
@@ -128,22 +128,6 @@ resource "aws_security_group" "pexip_management_sg" {
   )
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "UI access"
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "UI access"
-  }
-
-  ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
