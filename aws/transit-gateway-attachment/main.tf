fb2bc7ad15f0e34b8ed0bf1fe46ac073fbe9136a
Angelos Kyratzakos <angelos.kyratzakos@mattermost.com>
2024-09-30 18:52:00 +0300

    add security_group_referencing_support for aws_ec2_transit_gateway_vpc_attachment and setting default to disable (#749)

--- aws/transit-gateway-attachment/main.tf
+++ aws/transit-gateway-attachment/main.tf
@@ -1,7 +1,8 @@
 resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
-  subnet_ids         = var.subnet_ids
-  transit_gateway_id = var.transit_gateway_id
-  vpc_id             = var.vpc_id
+  subnet_ids                         = var.subnet_ids
+  transit_gateway_id                 = var.transit_gateway_id
+  vpc_id                             = var.vpc_id
+  security_group_referencing_support = var.security_group_referencing_support
 
   tags = {
     Name = var.name
