0e39d048344b97af2198ade42f058de96c42c28b
Angelos Kyratzakos <angelos.kyratzakos@mattermost.com>
2024-11-13 15:00:14 +0200

    add ability to enable versioning for s3 data bucket

--- aws/s3-data/variables.tf
+++ aws/s3-data/variables.tf
@@ -7,3 +7,9 @@ variable "iam_user_name" {
   type        = string
   description = "The name of the IAM user to access the bucket"
 }
+
+variable "s3_bucket_enable_versioning" {
+  type        = string
+  default     = true
+  description = "If the s3 bucket versioning to be enabled"
+}
