5a8e99b0f204dc1207a42daf05dc917ddc876f09
Andre Leite <andre.leite@mattermost.com>
2025-01-22 11:17:32 -0300

    CLD-8806: Fix thanos grpc route53 record

--- aws/eks-customer/route53.tf
+++ aws/eks-customer/route53.tf
@@ -9,7 +9,7 @@ resource "aws_route53_record" "internal" {
   for_each = toset(local.enabled_dns_names)
 
   zone_id = data.aws_route53_zone.internal.zone_id
-  name    = "${module.eks.cluster_name}.${each.value}"
+  name    = strcontains(each.value, "grpc") ? "${module.eks.cluster_name}-${each.value}" : "${module.eks.cluster_name}.${each.value}"
   type    = "CNAME"
   ttl     = 300
   records = strcontains(each.value, "grpc") ? [data.aws_lb.thanos-query-grpc.dns_name] : [data.aws_lb.internal.dns_name]
