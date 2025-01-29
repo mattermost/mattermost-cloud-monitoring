2ff7dfde698b4dc82626d480542aceb7486b99ed
Antonis Stamatiou <stamatiou.antonis@gmail.com>
2025-01-29 11:30:52 +0200

    fix: Generate Github token from JWT

--- aws/eks-customer/remove-utility.tf
+++ aws/eks-customer/remove-utility.tf
@@ -1,13 +1,14 @@
 resource "null_resource" "remove-utilities" {
   count = var.node_groups != {} ? 1 : 0
   triggers = {
-    gitops_repo_path     = var.gitops_repo_path
-    gitops_repo_url      = var.gitops_repo_url
-    gitops_repo_username = var.gitops_repo_username
-    gitops_repo_email    = var.gitops_repo_email
-    github_token         = data.github_app_token.this.token
-    environment          = var.environment
-    cluster_name         = module.eks.cluster_name
+    gitops_repo_path           = var.gitops_repo_path
+    gitops_repo_url            = var.gitops_repo_url
+    gitops_repo_username       = var.gitops_repo_username
+    gitops_repo_email          = var.gitops_repo_email
+    github_token               = data.github_app_token.this.token
+    github_app_installation_id = var.github_app_installation_id
+    environment                = var.environment
+    cluster_name               = module.eks.cluster_name
   }
   provisioner "local-exec" {
     when    = destroy
@@ -15,13 +16,14 @@ resource "null_resource" "remove-utilities" {
       bash ${path.module}/scripts/remove-utility.sh
     EOT
     environment = {
-      GIT_REPO_PATH     = self.triggers.gitops_repo_path
-      GIT_REPO_URL      = self.triggers.gitops_repo_url
-      GIT_REPO_USERNAME = self.triggers.gitops_repo_username
-      GIT_REPO_EMAIL    = self.triggers.gitops_repo_email
-      GITHUB_TOKEN      = self.triggers.github_token
-      CLUSTER_NAME      = self.triggers.cluster_name
-      ENV               = self.triggers.environment
+      GIT_REPO_PATH              = self.triggers.gitops_repo_path
+      GIT_REPO_URL               = self.triggers.gitops_repo_url
+      GIT_REPO_USERNAME          = self.triggers.gitops_repo_username
+      GIT_REPO_EMAIL             = self.triggers.gitops_repo_email
+      GITHUB_TOKEN               = self.triggers.github_token
+      GITHUB_APP_INSTALLATION_ID = self.triggers.github_app_installation_id
+      CLUSTER_NAME               = self.triggers.cluster_name
+      ENV                        = self.triggers.environment
     }
   }
 }
