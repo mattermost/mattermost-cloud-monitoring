44c8d571438b18b942b0a76723ae9a69f9f74bbe
Antonis Stamatiou <stamatiou.antonis@gmail.com>
2025-02-07 12:02:12 +0200

    fix: Add missing region on Github IAM Role (#816)

--- aws/github-runner/locals.tf
+++ aws/github-runner/locals.tf
@@ -1,5 +1,7 @@
 data "aws_caller_identity" "current" {}
 
+data "aws_region" "current" {}
+
 # Fetch the default KMS key for Secrets Manager
 data "aws_kms_key" "default_secrets_manager_key" {
   key_id = "alias/aws/secretsmanager"
