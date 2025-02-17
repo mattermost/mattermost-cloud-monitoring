fd382a74de1c581450a2d5f26d7b24643a4255ba
Stavros Foteinopoulos <stafot@gmail.com>
2025-02-17 18:19:56 +0200

    Refactor calico support in cnc cluster module (#821)
    Signed-off-by: Stavros Foteinopoulos <stafot@gmail.com>

--- aws/cluster/providers.tf
+++ aws/cluster/providers.tf
@@ -21,6 +21,14 @@ terraform {
       source  = "hashicorp/http"
       version = "~> 2.2"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.2.2"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.5.1"
+    }
   }
 }
 
