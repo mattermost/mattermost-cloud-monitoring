229704c95d6d39c16e367240461394649ea30966
Angelos Kyratzakos <angelos.kyratzakos@mattermost.com>
2025-04-11 12:33:29 +0200

    Add initial setup for Pexip deployment with EC2 instances, ELBs, DNS records, security groups, and variables (#851)
    - Create resources for management and conference EC2 instances, including network interfaces and Elastic IP associations.
    - Implement Elastic Load Balancers (ELBs) for management and conference nodes with appropriate listeners and health checks.
    - Add Cloudflare DNS records for the conference nodes.
    - Define security groups for management and conference nodes with specific ingress and egress rules.
    - Establish necessary variables for configuration and deployment.

---
+++ aws/pexip-dual/dns_records.tf
@@ -0,0 +1,35 @@
+resource "cloudflare_dns_record" "pexip_conference_first" {
+  zone_id = var.cloudflare_zone_id
+  name    = var.conference_cloudflare_record_name_first
+  content = aws_elb.pexip_conference_elb_first.dns_name
+  type    = "CNAME"
+  proxied = true
+  ttl     = 1 # 1 means 'automatic' since we're using proxied=true
+  comment = "Pexip conference node DNS record for first node"
+}
+
+resource "cloudflare_dns_record" "pexip_conference_second" {
+  zone_id = var.cloudflare_zone_id
+  name    = var.conference_cloudflare_record_name_second
+  content = aws_elb.pexip_conference_elb_second.dns_name
+  type    = "CNAME"
+  proxied = true
+  ttl     = 1 # 1 means 'automatic' since we're using proxied=true
+  comment = "Pexip conference node DNS record for second node"
+}
+
+data "aws_route53_zone" "private_zone" {
+  name         = "internal.${var.environment}.cloud.mattermost.com"
+  private_zone = true
+}
+
+resource "aws_route53_record" "pexip_management" {
+  zone_id = data.aws_route53_zone.private_zone.zone_id
+  name    = "${var.management_route53_record_name}.${data.aws_route53_zone.private_zone.name}"
+  type    = "A"
+  alias {
+    name                   = aws_elb.pexip_management_elb.dns_name
+    zone_id                = aws_elb.pexip_management_elb.zone_id
+    evaluate_target_health = true
+  }
+}
