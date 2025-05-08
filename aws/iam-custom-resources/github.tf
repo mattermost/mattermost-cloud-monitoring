0d3bee4b818920e72c18ffc41c19cf93f051e8fa
Stavros Foteinopoulos <stafot@gmail.com>
2025-05-08 12:19:17 +0300

    Update github.tf

--- aws/iam-custom-resources/github.tf
+++ aws/iam-custom-resources/github.tf
@@ -1,8 +1,3 @@
-variable "environment" {
-  type        = string
-  description = "Environment name for the OIDC provider"
-}
-
 resource "aws_iam_openid_connect_provider" "github_oidc" {
   url            = "https://token.actions.githubusercontent.com"
   client_id_list = ["sts.amazonaws.com"]
