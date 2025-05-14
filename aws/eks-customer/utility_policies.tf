6e219e67e2c4000c1cf602790f3a487824708ffd
Angelos Kyratzakos <angelos.kyratzakos@mattermost.com>
2025-05-14 17:23:27 +0200

    fix typo

--- aws/eks-customer/utility_policies.tf
+++ aws/eks-customer/utility_policies.tf
@@ -105,7 +105,7 @@ resource "aws_iam_policy" "external-secrets" {
             "Effect": "Allow",
             "Resource": [
                 "arn:aws:secretsmanager:us-east-1:${data.aws_caller_identity.current.account_id}:secret:app-bifrost-*",
-                "arn:aws:secretsmanager:us-east-1:${data.aws_caller_identity.current.account_id}:secret:app-external-dns-public-*"
+                "arn:aws:secretsmanager:us-east-1:${data.aws_caller_identity.current.account_id}:secret:app-external-dns-public-*",
                 "arn:aws:secretsmanager:us-east-1:${data.aws_caller_identity.current.account_id}:secret:app-wiz-kubernetes-integration-*"
             ]
         }
