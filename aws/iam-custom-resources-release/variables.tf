b49c7e10cca30486b26082bbe14f8b58094bcb32
Angelos Kyratzakos <angelos.kyratzakos@mattermost.com>
2025-06-11 12:48:25 +0200

    linting / docs

--- aws/iam-custom-resources-release/variables.tf
+++ aws/iam-custom-resources-release/variables.tf
@@ -3,12 +3,6 @@ variable "environment" {
   type        = string
 }
 
-
-variable "github_runners_iam_role_arn" {
-  description = "Github runner role ARN"
-  type        = string
-}
-
 variable "github_repos_sub" {
   description = "Github repos sub"
   type        = list(string)
