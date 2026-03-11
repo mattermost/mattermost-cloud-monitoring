2a70df96e37fc57dc837576869f6253da5c9eafe
Angelos Kyratzakos <angelos.kyratzakos@mattermost.com>
2026-03-11 15:30:04 +0100

    Enhance Pexip management configurations for public access
    - Updated EC2 instance and network interface settings to conditionally use public subnet and allocate Elastic IP for management nodes based on the management_public variable.
    - Modified DNS record settings to enable proxying and adjust TTL based on initial configuration.
    - Added dynamic listener for SSH access in the ELB resource during initial configuration.
    - Enhanced security group rules to allow public access and SSH from the ELB for initial setup, improving flexibility and security for management access.

--- aws/pexip-multi-conferences/elb.tf
+++ aws/pexip-multi-conferences/elb.tf
@@ -12,6 +12,16 @@ resource "aws_elb" "pexip_management_elb" {
     ssl_certificate_id = var.management_public ? var.elb_ssl_certificate_arn_public : var.elb_ssl_certificate_arn_internal
   }
 
+  dynamic "listener" {
+    for_each = var.initial_configuration ? [1] : []
+    content {
+      instance_port     = 22
+      instance_protocol = "tcp"
+      lb_port           = 22
+      lb_protocol       = "tcp"
+    }
+  }
+
   health_check {
     healthy_threshold   = 2
     unhealthy_threshold = 2
